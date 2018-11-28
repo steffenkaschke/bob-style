ee7a1166a569150f7c8ab9d8317d6e0523aab643
Doron Cyngiser <doron.cy@gmail.com>
2018-11-28 14:49:17 +0200

    updated the avara component

--- projects/ui-framework/src/lib/avatar/avatar.stories.ts
+++ projects/ui-framework/src/lib/avatar/avatar.stories.ts
@@ -2,17 +2,31 @@ import { storiesOf } from '@storybook/angular';
 import { withNotes } from '@storybook/addon-notes';
 import { text, select, withKnobs } from '@storybook/addon-knobs/angular';
 import { action } from '@storybook/addon-actions';
-import { AvatarComponent } from './avatar.component';
+import { AvatarComponent, AvatarSize } from './avatar.component';
+import { values } from 'lodash';
 
 const avatarStories = storiesOf('Buttons & Indicators|Avatar', module)
   .addDecorator(withNotes)
   .addDecorator(withKnobs);
 
+const sizeOptions = values(AvatarSize);
 const note = `
-  # hi
-  ~~~~
-  <b-avatar></b-avatar>
-  ~~~~
+  ## Avatar Element
+  ##### Basic component for display image avatar
+
+  Name | Type | Description | Default value
+  --- | --- | --- | ---
+  imageSource | String | url of the image |
+  size | AvatarSize | enum for setting the avatar size | mini (optional)
+  handleClick | Function | callback for clicking on the avatar | no click (optional)
+
+  ~~~
+  <b-avatar
+    [imageSource] = "imageUrl"
+    [size] = "medium"
+    (handleClick) = "clickHandler()">
+  </b-avatar>
+  ~~~
 `;
 avatarStories.add(
     'Not Clickable',
@@ -21,7 +35,7 @@ avatarStories.add(
         component: AvatarComponent,
         props: {
           imageSource: text('imageSource', 'https://pixel.nymag.com/imgs/daily/vulture/2017/03/23/23-han-solo.w330.h330.jpg'),
-          size: select('size', ['mini', 'small', 'medium', 'large'], 'small')
+          size: select('size', sizeOptions, AvatarSize.small)
         }
       };
     },
@@ -33,10 +47,10 @@ avatarStories.add(
       component: AvatarComponent,
         props: {
           imageSource: text('imageSource', 'https://pixel.nymag.com/imgs/daily/vulture/2017/03/23/23-han-solo.w330.h330.jpg'),
-          size: select('size', ['mini', 'small', 'medium', 'large'], 'small'),
-          handleClick: action('log 1'),
+          size: select('size', sizeOptions, AvatarSize.small),
+          handleClick: action(),
         },
     }),
-    { notes: `` }
+    { notes: { markdown: note }  }
   );
 
