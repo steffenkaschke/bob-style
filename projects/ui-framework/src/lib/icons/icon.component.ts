efd09f4dda45df9458665ee82556b6d9f95bf97b
Doron Cyngiser <doron.cy@gmail.com>
2018-12-13 13:51:01 +0200

    added icon element

---
+++ projects/ui-framework/src/lib/icons/icon.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IconService } from './icon.service';
+import { Icons, IconSize, IconColor } from './icons.enum';
+
+@Component({
+  selector: 'b-icon',
+  template: `
+  <div fxLayout="row" fxLayoutAlign="center center"
+    [matTooltip]="toolTipSummary" [matTooltipPosition]="'above'" [matTooltipShowDelay]="300">
+    <mat-icon [svgIcon]="icon" [ngClass]="getClassNames()"></mat-icon>
+  </div>
+  `,
+  styleUrls: ['./icon.component.scss'],
+})
+export class IconComponent implements OnInit {
+  @Input() icon: Icons;
+  @Input() size: IconSize;
+  @Input() color: IconColor;
+  @Input() toolTipSummary: string;
+
+  constructor(private iconService: IconService) {
+  }
+
+  ngOnInit(): void {
+    this.iconService.initIcon(this.icon);
+  }
+
+  getClassNames() {
+    return `${this.size} ${this.color}`;
+  }
+}
