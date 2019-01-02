969eb8145d907e761f275c9189b8ada9fa8f878a
doroncy <doron.cy@gmail.com>
2019-01-02 15:12:44 +0200

    rearrange

--- projects/ui-framework/src/lib/ui-framework.module.ts
+++ projects/ui-framework/src/lib/ui-framework.module.ts
@@ -1,35 +1,9 @@
-import { NgModule, ModuleWithProviders, InjectionToken } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { UIFrameworkConfig } from './ui-framework.types';
-import { AvatarComponent } from './avatar/avatar.component';
-import { ButtonsModule } from './buttons/buttons.module';
-import { TypographyModule } from './typography/typography.module';
-import { IconsModule } from './icons/icons.module';
-import { InputModule } from './input/input.module';
+import { NgModule } from '@angular/core';
+import './style/style.scss';
 
-export const UI_FRAMEWORK_CONFIG = new InjectionToken<UIFrameworkConfig>('UI Framework config');
 @NgModule({
-  declarations: [
-    AvatarComponent,
-  ],
-  imports: [
-    BrowserModule,
-    ButtonsModule,
-    TypographyModule,
-    IconsModule,
-    InputModule,
-  ],
-  exports: [
-    AvatarComponent,
-  ]
+  declarations: [],
+  imports: [],
+  exports: []
 })
-export class UIFrameworkModule {
-  static init(config: UIFrameworkConfig): ModuleWithProviders {
-    return {
-      ngModule: UIFrameworkModule,
-      providers: [{
-        provide: UI_FRAMEWORK_CONFIG, useValue: config
-      }]
-    };
-  }
-}
+export class UIFrameworkModule {}
--- projects/ui-framework/src/lib/ui-framework.types.ts
+++
@@ -1,3 +0,0 @@
-export interface UIFrameworkConfig {
-  iconsUrl: string;
-}
