7b70db8089bdc6e820d476399c8f3bada9902285
Ishai <ishaiborovoy@gmail.com>
2019-01-16 15:58:24 +0200

    Try to fix template issue by provide both templateUrl and template (#109)

--- projects/ui-framework/src/lib/form-elements/datepicker/datepicker-input.component.ts
+++ projects/ui-framework/src/lib/form-elements/datepicker/datepicker-input.component.ts
@@ -3,6 +3,7 @@ import { InputComponent } from '../input/input.component';
 
 @Component({
   selector: 'b-datepicker-input',
+  templateUrl: '../input/input.component.html',
   template: InputComponent.addAttributesToBaseInput('[matDatepicker]="datePickerId"'),
   styleUrls: ['../input/input.component.scss']
 })
