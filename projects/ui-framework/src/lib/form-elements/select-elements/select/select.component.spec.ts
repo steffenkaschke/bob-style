5c007c90aa69face99f80130437942909440a311
Omri Hecht <omrihecht@gmail.com>
2019-01-17 08:31:31 +0200

    add select tests (#112)

---
+++ projects/ui-framework/src/lib/form-elements/select-elements/select/select.component.spec.ts
@@ -0,0 +1,283 @@
+import { async, ComponentFixture, fakeAsync, flush, inject, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { CommonModule } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SelectComponent } from './select.component';
+import { SelectModelService } from './select-model.service';
+import { SearchModule } from '../../../search/search.module';
+import { ButtonsModule } from '../../../buttons';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Platform } from '@angular/cdk/platform';
+
+describe('SelectComponent', () => {
+  let component: SelectComponent;
+  let optionsMock;
+  let selectionGroupOptionsMock;
+  let fixture: ComponentFixture<SelectComponent>;
+  let overlayContainer: OverlayContainer;
+  let overlayContainerElement: HTMLElement;
+  let platform: Platform;
+
+  beforeEach(async(() => {
+    optionsMock = [
+      {
+        groupName: 'Basic Info',
+        options: [
+          { value: 'Basic Info 1', id: 1 },
+          { value: 'Basic Info 2', id: 2 },
+        ],
+      },
+      {
+        groupName: 'Personal',
+        options: [
+          { value: 'Personal 1', id: 11 },
+          { value: 'Personal 2', id: 12 },
+        ],
+      },
+    ];
+
+    selectionGroupOptionsMock = [
+      {
+        groupName: 'Basic Info',
+        isCollapsed: false,
+        groupHeader: {
+          groupName: 'Basic Info',
+          value: 'Basic Info',
+          id: 'Basic Info',
+          isGroupHeader: true,
+        },
+        'options': [
+          {
+            value: 'Basic Info 1',
+            id: 1,
+            groupName: 'Basic Info',
+            isGroupHeader: false,
+          },
+          {
+            value: 'Basic Info 2',
+            id: 2,
+            groupName: 'Basic Info',
+            isGroupHeader: false,
+          },
+        ]
+      },
+      {
+        groupName: 'Personal',
+        isCollapsed: false,
+        groupHeader: {
+          groupName: 'Personal',
+          value: 'Personal',
+          id: 'Personal',
+          isGroupHeader: true,
+        },
+        'options': [
+          {
+            value: 'Personal 1',
+            id: 11,
+            groupName: 'Personal',
+            isGroupHeader: false,
+          },
+          {
+            value: 'Personal 2',
+            id: 12,
+            groupName: 'Personal',
+            isGroupHeader: false
+          },
+        ]
+      },
+    ];
+
+    TestBed.configureTestingModule({
+      declarations: [
+        SelectComponent,
+      ],
+      providers: [
+        SelectModelService,
+      ],
+      imports: [
+        NoopAnimationsModule,
+        CommonModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatSelectModule,
+        SearchModule,
+        ButtonsModule,
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+      });
+
+    inject([OverlayContainer, Platform], (oc: OverlayContainer, p: Platform) => {
+      overlayContainer = oc;
+      overlayContainerElement = oc.getContainerElement();
+      platform = p;
+    })();
+  }));
+
+
+  describe('multiSelect', () => {
+    beforeEach(async(() => {
+      fixture = TestBed.createComponent(SelectComponent);
+      component = fixture.componentInstance;
+      component.options = optionsMock;
+      component.isMultiSelect = true;
+      component.selectedIds = [1, 2];
+      fixture.detectChanges();
+    }));
+    it('should set the selectionGroupOptions', () => {
+      expect(component.selectionGroupOptions).toEqual(selectionGroupOptionsMock);
+    });
+    it('should set the selectedModel', () => {
+      expect(component.selectedModel).toEqual([
+        selectionGroupOptionsMock[0].options[0],
+        selectionGroupOptionsMock[0].options[1],
+        selectionGroupOptionsMock[0].groupHeader,
+      ]);
+    });
+    it('should set the triggerValue', () => {
+      expect(component.triggerValue).toEqual('Basic Info 1, Basic Info 2, Basic Info');
+    });
+    describe('onOptionClick', () => {
+      it('should add all options when clicking unselected headerOption',
+        fakeAsync(() => {
+          component.mySelect.open();
+          fixture.detectChanges();
+          flush();
+
+          (overlayContainerElement.querySelectorAll('mat-option')[3] as HTMLElement).click();
+          fixture.detectChanges();
+          flush();
+
+          expect(component.selectedModel).toEqual([
+            selectionGroupOptionsMock[0].groupHeader,
+            selectionGroupOptionsMock[0].options[0],
+            selectionGroupOptionsMock[0].options[1],
+            selectionGroupOptionsMock[1].groupHeader,
+            selectionGroupOptionsMock[1].options[0],
+            selectionGroupOptionsMock[1].options[1],
+          ]);
+        }));
+      it('should remove all options when clicking selected headerOption',
+        fakeAsync(() => {
+          component.mySelect.open();
+          fixture.detectChanges();
+          flush();
+
+          (overlayContainerElement.querySelectorAll('mat-option')[0] as HTMLElement).click();
+          fixture.detectChanges();
+          flush();
+
+          expect(component.selectedModel).toEqual([]);
+        }));
+      it('should select headerOption if group option is selected and other all group options are selected',
+        fakeAsync(() => {
+          component.mySelect.open();
+          fixture.detectChanges();
+          flush();
+
+          (overlayContainerElement.querySelectorAll('mat-option')[4] as HTMLElement).click();
+          fixture.detectChanges();
+          flush();
+
+          expect(component.selectedModel).toEqual([
+            selectionGroupOptionsMock[0].options[0],
+            selectionGroupOptionsMock[0].options[1],
+            selectionGroupOptionsMock[1].options[0],
+            selectionGroupOptionsMock[0].groupHeader,
+          ]);
+
+          (overlayContainerElement.querySelectorAll('mat-option')[5] as HTMLElement).click();
+          fixture.detectChanges();
+          flush();
+
+          expect(component.selectedModel).toEqual([
+            selectionGroupOptionsMock[0].options[0],
+            selectionGroupOptionsMock[0].options[1],
+            selectionGroupOptionsMock[1].options[0],
+            selectionGroupOptionsMock[1].options[1],
+            selectionGroupOptionsMock[0].groupHeader,
+            selectionGroupOptionsMock[1].groupHeader,
+          ]);
+        }));
+
+      it('should unselect headerOption if group option is unselected',
+        fakeAsync(() => {
+          component.mySelect.open();
+          fixture.detectChanges();
+          flush();
+
+          (overlayContainerElement.querySelectorAll('mat-option')[1] as HTMLElement).click();
+          fixture.detectChanges();
+          flush();
+
+          expect(component.selectedModel).toEqual([
+            selectionGroupOptionsMock[0].options[1],
+          ]);
+        }));
+
+      it('should update triggerText',
+        fakeAsync(() => {
+          component.mySelect.open();
+          fixture.detectChanges();
+          flush();
+
+          (overlayContainerElement.querySelectorAll('mat-option')[4] as HTMLElement).click();
+          fixture.detectChanges();
+          flush();
+
+          expect(component.triggerValue).toEqual('Basic Info 1, Basic Info 2, Personal 1, Basic Info');
+        }));
+    });
+  });
+
+  describe('singleSelect', () => {
+    beforeEach(async(() => {
+      fixture = TestBed.createComponent(SelectComponent);
+      component = fixture.componentInstance;
+      component.options = optionsMock;
+      component.isMultiSelect = false;
+      component.selectedIds = [1];
+      fixture.detectChanges();
+    }));
+    it('should set the selectionGroupOptions', () => {
+      expect(component.selectionGroupOptions).toEqual(selectionGroupOptionsMock);
+    });
+    it('should set the selectedModel', () => {
+      expect(component.selectedModel).toEqual(selectionGroupOptionsMock[0].options[0]);
+    });
+    it('should set the triggerValue', () => {
+      expect(component.triggerValue).toEqual('Basic Info 1');
+    });
+    describe('onOptionClick', () => {
+      it('should change selectedModel with the selected option',
+        fakeAsync(() => {
+          component.mySelect.open();
+          fixture.detectChanges();
+          flush();
+
+          (overlayContainerElement.querySelectorAll('mat-option')[4] as HTMLElement).click();
+          fixture.detectChanges();
+          flush();
+
+          expect(component.selectedModel).toEqual(selectionGroupOptionsMock[1].options[0]);
+        }));
+
+      it('should update triggerText',
+        fakeAsync(() => {
+          component.mySelect.open();
+          fixture.detectChanges();
+          flush();
+
+          (overlayContainerElement.querySelectorAll('mat-option')[4] as HTMLElement).click();
+          fixture.detectChanges();
+          flush();
+
+          expect(component.triggerValue).toEqual('Personal 1');
+        }));
+    });
+  });
+});
