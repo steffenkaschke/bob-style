4117d9fb3c31fc16d88cbf54cd099181157a3b2b
Omri Hecht <omrihecht@gmail.com>
2019-02-05 10:17:03 +0200

    auto complete style (#160)

--- projects/ui-framework/src/lib/form-elements/auto-complete/auto-complete.stories.ts
+++ projects/ui-framework/src/lib/form-elements/auto-complete/auto-complete.stories.ts
@@ -1,18 +1,23 @@
 import { storiesOf } from '@storybook/angular';
 import { withNotes } from '@storybook/addon-notes';
-import { text, select, boolean, withKnobs, number } from '@storybook/addon-knobs/angular';
+import { text, select, boolean, withKnobs, number, object } from '@storybook/addon-knobs/angular';
 import { action } from '@storybook/addon-actions';
 import { values } from 'lodash';
 import { ComponentGroupType } from '../../consts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AutoCompleteModule } from './auto-complete.module';
+import { TypographyModule } from '../../typography/typography.module';
+import { SelectGroupOption } from '../select';
 
 const textareaStories = storiesOf(ComponentGroupType.FormElements, module)
   .addDecorator(withNotes)
   .addDecorator(withKnobs);
 
 const template = `
+<b-display-3>Auto complete</b-display-3>
+
 <b-auto-complete style="display:block; width: 400px;"
+                [options]="options"
                 [label]="label"
                 [value]="value"
                 [disabled]="disabled"
@@ -23,6 +28,28 @@ const template = `
 </b-auto-complete>
 `;
 
+const optionsMock = [
+  {
+    id: '1789840653568443152',
+    value: 'Pablo Escobar',
+    avatar: 'https://cdn.filestackcontent.com/zVtpbknNTF6pQyzdnxNM?policy=eyJoYW5kbGUiOiJ6VnRwYmtuTlRGNnBReXpkbnhOTSIsImV4cGlyeSI6NDYzNTkxODcwNCwiY2FsbCI6WyJyZWFkIiwgImNvbnZlcnQiXX0=&signature=db8ddcad4c8bcaa1f08a46f5c43a3748764e6d9c0d6cdd9aee7eaf1996c96d63'
+  },
+  {
+    id: '1720274302193894094',
+    value: 'Tia Davidson',
+    avatar: 'https://cdn.filepicker.io/api/file/mSZcYn4iRomv8SZg3W8a?signature=a1c1514dd4447b2e2e0316bda9219462e8b792dd4ebdbe79e89b8e2d864cdcb0&policy=eyJoYW5kbGUiOiJtU1pjWW40aVJvbXY4U1pnM1c4YSIsImV4cGlyeSI6NDYzNTkxODcwNH0='
+  },
+  {
+    id: '1720274301447307981',
+    value: 'Sarah Gill',
+    avatar: 'https://cdn.filepicker.io/api/file/MXMs1ab9TiENC3OZWBjo?signature=3668190ee11cc0e12728e7042e22a2bca055c8cf9a53030ecde4d7d46d9a2db2&policy=eyJoYW5kbGUiOiJNWE1zMWFiOVRpRU5DM09aV0JqbyIsImV4cGlyeSI6NDYzNTkxODcwNH0='
+  },
+  {
+    id: '1720274305876493012',
+    value: 'Joy Vance',
+    avatar: 'https://cdn.filepicker.io/api/file/8SBH9wxTQCeFszcPB4kz?signature=5de409f4112d5d0c255474231c15254635124fc64e0d155bbf951804f3d27179&policy=eyJoYW5kbGUiOiI4U0JIOXd4VFFDZUZzemNQQjRreiIsImV4cGlyeSI6NDYzNTkxODcwNH0='
+  },
+];
 
 const note = `
   ## Auto complete Element
@@ -47,6 +74,7 @@ textareaStories.add(
     return {
       template,
       props: {
+        options: object<SelectGroupOption>('options', optionsMock),
         onSelect: action(),
         value: text('value', ''),
         label: text('label', 'label text'),
@@ -59,6 +87,7 @@ textareaStories.add(
         imports: [
           BrowserAnimationsModule,
           AutoCompleteModule,
+          TypographyModule,
         ]
       }
     };
