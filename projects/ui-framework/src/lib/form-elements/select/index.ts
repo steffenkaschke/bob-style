61d4e2005916aff5be5def48e4584aac60c47c7c
Omri Hecht <omrihecht@gmail.com>
2019-02-12 22:21:35 +0200

    remove old select (#187)

--- projects/ui-framework/src/lib/form-elements/select/index.ts
+++
@@ -1,4 +0,0 @@
-// export { SelectGroupOption } from './select.interface';
-// export * from './select.module';
-// export * from './single-select/single-select.component';
-// export * from './multi-select/multi-select.component';
