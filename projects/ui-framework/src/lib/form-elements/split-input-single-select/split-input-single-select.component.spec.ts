841d3540c191adf55688e242330d0db4a153f9c4
Omri Hecht <omrihecht@gmail.com>
2019-02-17 16:04:27 +0200

    Omri/split input single select (#204)
    * split input single select
    
    * split input single select

---
+++ projects/ui-framework/src/lib/form-elements/split-input-single-select/split-input-single-select.component.spec.ts
@@ -0,0 +1,135 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SplitInputSingleSelectComponent } from './split-input-single-select.component';
+import { SingleSelectComponent } from '../lists/single-select/single-select.component';
+import { MockComponent } from 'ng-mocks';
+import { InputComponent, InputEventType } from '../input';
+import { SelectGroupOption } from '../lists';
+import { InputSingleSelectValue } from './split-input-single-select.interface';
+import { By } from '@angular/platform-browser';
+
+describe('SplitInputSingleSelectComponent', () => {
+  let component: SplitInputSingleSelectComponent;
+  let fixture: ComponentFixture<SplitInputSingleSelectComponent>;
+  const optionsMock: SelectGroupOption[] = [
+    {
+      groupName: 'currencies',
+      options: [
+        { value: 'USD', id: 'USD' },
+        { value: 'GBP', id: 'GBP' },
+        { value: 'ILS', id: 'ILS' },
+      ],
+    },
+  ];
+
+  const valueMock: InputSingleSelectValue = {
+    inputValue: 200,
+    selectValue: 'USD',
+  };
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        SplitInputSingleSelectComponent,
+        MockComponent(SingleSelectComponent),
+        MockComponent(InputComponent),
+      ],
+      imports: [
+        NoopAnimationsModule,
+        CommonModule,
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(SplitInputSingleSelectComponent);
+        component = fixture.componentInstance;
+        spyOn(component.elementChange, 'emit');
+        fixture.detectChanges();
+      });
+  }));
+
+  describe('OnChanges', () => {
+    it('should set default value object with null values if no value is provided', () => {
+      component.ngOnChanges({});
+      expect(component.value).toEqual({
+        inputValue: null,
+        selectValue: null,
+      });
+    });
+    it('should use the provided value and not overwrite with nulls', () => {
+      component.value = valueMock;
+      component.ngOnChanges({});
+      expect(component.value).toEqual(valueMock);
+    });
+    it('should set inputValue as value on the inputEl', () => {
+      component.value = valueMock;
+      component.ngOnChanges({});
+      fixture.detectChanges();
+      const inputEl = fixture.debugElement.query(By.css('b-input'));
+      expect(inputEl.context.value).toEqual(valueMock.inputValue);
+    });
+    it('should set selectValue as value on the selectEl', () => {
+      component.value = valueMock;
+      component.ngOnChanges({});
+      fixture.detectChanges();
+      const selectEl = fixture.debugElement.query(By.css('b-single-select'));
+      expect(selectEl.context.value).toEqual(valueMock.selectValue);
+    });
+    it('should set options on select element', () => {
+      component.selectOptions = optionsMock;
+      component.ngOnChanges({});
+      fixture.detectChanges();
+      const selectEl = fixture.debugElement.query(By.css('b-single-select'));
+      expect(selectEl.context.options).toEqual(optionsMock);
+    });
+  });
+
+  describe('onInputChange', () => {
+    beforeEach(() => {
+      component.selectOptions = optionsMock;
+      component.value = valueMock;
+      component.ngOnChanges({});
+      fixture.detectChanges();
+    });
+    it('should not emit event if inputEvent is not change', () => {
+      const inputEl = fixture.debugElement.query(By.css('b-input'));
+      inputEl.context.inputEvents.emit({
+        event: InputEventType.onBlur,
+        value: 10,
+      });
+      fixture.detectChanges();
+      expect(component.elementChange.emit).not.toHaveBeenCalled();
+    });
+    it('should update value and emit event with updated value', () => {
+      const inputEl = fixture.debugElement.query(By.css('b-input'));
+      inputEl.context.inputEvents.emit({
+        event: InputEventType.onChange,
+        value: 500,
+      });
+      fixture.detectChanges();
+      expect(component.elementChange.emit).toHaveBeenCalledWith({
+        inputValue: 500,
+        selectValue: 'USD',
+      });
+    });
+  });
+
+  describe('onSelectChange', () => {
+    beforeEach(() => {
+      component.selectOptions = optionsMock;
+      component.value = valueMock;
+      component.ngOnChanges({});
+      fixture.detectChanges();
+    });
+    it('should update value and emit event with updated value', () => {
+      const selectEl = fixture.debugElement.query(By.css('b-single-select'));
+      selectEl.context.selectChange.emit('GBP');
+      fixture.detectChanges();
+      expect(component.elementChange.emit).toHaveBeenCalledWith({
+        inputValue: 200,
+        selectValue: 'GBP',
+      });
+    });
+  });
+});
--- projects/ui-framework/src/lib/form-elements/split-input-single-select/split-input-single-select.stories.ts
+++ projects/ui-framework/src/lib/form-elements/split-input-single-select/split-input-single-select.stories.ts
@@ -1,20 +1,33 @@
 import { storiesOf } from '@storybook/angular';
 import { withNotes } from '@storybook/addon-notes';
-import { boolean, number, select, text, withKnobs } from '@storybook/addon-knobs/angular';
+import { boolean, number, object, select, text, withKnobs } from '@storybook/addon-knobs/angular';
 import { action } from '@storybook/addon-actions';
 import { values } from 'lodash';
 import { ComponentGroupType } from '../../consts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoryBookLayoutModule } from '../../story-book-layout/story-book-layout.module';
 import { SplitInputSingleSelectModule } from './split-input-single-select.module';
+import { SelectGroupOption } from '../lists/list.interface';
+import { InputTypes } from '../input/input.enum';
+import map from 'lodash/map';
+import { InputSingleSelectValue } from './split-input-single-select.interface';
 
 const textareaStories = storiesOf(ComponentGroupType.FormElements, module)
   .addDecorator(withNotes)
   .addDecorator(withKnobs);
 
 const template = `
-<b-currency-value style="width: 400px;">
-</b-currency-value>
+<b-split-input-single-select style="width: 400px;"
+                             [hintMessage]="hintMessage"
+                             [errorMessage]="errorMessage"
+                             [disabled]="disabled"
+                             [required]="required"
+                             [label]="label"
+                             [value]="value"
+                             [inputType]="inputType"
+                             [selectOptions]="selectOptions"
+                             (elementChange)="elementChange($event)">
+</b-split-input-single-select>
 `;
 
 const storyTemplate = `
@@ -30,32 +43,106 @@ const note = `
 
   Name | Type | Description
   --- | --- | ---
-  currencyValue | CurrencyValue | currency value as CurrencyValue
+  value | SplitInputSingleSelectValue | value of the input and select
+  selectOptions | SelectGroupOption[] | the options model for the select element
   label | string | label text
   disabled | boolean | is field disabled
   required | boolean | is field required
   hintMessage | text | hint text
   errorMessage | text | error text
-  currencyChange | Action | currency change event emitter
+  elementChange | Action | element change emitter
 
   ~~~
   ${ template }
   ~~~
 `;
+
+
+const currencies = [
+  { value: 'AED', serverId: null },
+  { value: 'ANG', serverId: null },
+  { value: 'AUD', serverId: null },
+  { value: 'AZN', serverId: null },
+  { value: 'BAM', serverId: null },
+  { value: 'BGN', serverId: null },
+  { value: 'BRL', serverId: null },
+  { value: 'BTC', serverId: null },
+  { value: 'BWP', serverId: null },
+  { value: 'CAD', serverId: null },
+  { value: 'CHF', serverId: null },
+  { value: 'CLP', serverId: null },
+  { value: 'CNY', serverId: null },
+  { value: 'COP', serverId: null },
+  { value: 'CZK', serverId: null },
+  { value: 'DKK', serverId: null },
+  { value: 'EGP', serverId: null },
+  { value: 'EUR', serverId: null },
+  { value: 'GBP', serverId: null },
+  { value: 'HKD', serverId: null },
+  { value: 'HUF', serverId: null },
+  { value: 'IDR', serverId: null },
+  { value: 'ILS', serverId: null },
+  { value: 'INR', serverId: null },
+  { value: 'JPY', serverId: null },
+  { value: 'KES', serverId: null },
+  { value: 'KRW', serverId: null },
+  { value: 'MAD', serverId: null },
+  { value: 'MMK', serverId: null },
+  { value: 'MXN', serverId: null },
+  { value: 'MYR', serverId: null },
+  { value: 'NGN', serverId: null },
+  { value: 'NOK', serverId: null },
+  { value: 'NPR', serverId: null },
+  { value: 'NZD', serverId: null },
+  { value: 'PEN', serverId: null },
+  { value: 'PHP', serverId: null },
+  { value: 'PLN', serverId: null },
+  { value: 'RON', serverId: null },
+  { value: 'RUB', serverId: null },
+  { value: 'SEK', serverId: null },
+  { value: 'SGD', serverId: null },
+  { value: 'THB', serverId: null },
+  { value: 'TRY', serverId: null },
+  { value: 'TWD', serverId: null },
+  { value: 'TZS', serverId: null },
+  { value: 'UAH', serverId: null },
+  { value: 'USD', serverId: null },
+  { value: 'UYU', serverId: null },
+  { value: 'VND', serverId: null },
+  { value: 'XOF', serverId: null },
+  { value: 'ZAR', serverId: null },
+];
+
+const optionsMock: SelectGroupOption[] = Array.from(Array(1), (_, i) => {
+  return {
+    groupName: 'all currencies',
+    options: map(currencies, currency => ({
+      value: currency.value,
+      id: currency.value,
+    })),
+  };
+});
+
+const value: InputSingleSelectValue = {
+  inputValue: 100,
+  selectValue: 'GBP',
+};
+
 textareaStories.add(
   'Split input single select',
   () => {
     return {
       template: storyTemplate,
       props: {
-        inputEvents: action(),
-        maxChars: number('maxChars', ''),
-        value: text('value', ''),
-        label: text('label', 'label text'),
+        value: object('value', value),
+        inputType: select('type', InputTypes, InputTypes.number),
+        selectOptions: object('selectOptions', optionsMock),
+        label: text('label', 'Base salary'),
         disabled: boolean('disabled', false),
         required: boolean('required', false),
         hintMessage: text('hintMessage', 'This field should contain something'),
         errorMessage: text('errorMessage', ''),
+        elementChange: action(),
       },
       moduleMetadata: {
         imports: [
