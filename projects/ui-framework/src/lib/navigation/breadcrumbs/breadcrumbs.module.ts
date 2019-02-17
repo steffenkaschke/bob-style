cd74d9b3b27075a5998edcdde65c6a32b277b849
doroncy <doron.cy@gmail.com>
2019-02-17 15:45:40 +0200

    add window resize event

--- projects/ui-framework/src/lib/navigation/breadcrumbs/breadcrumbs.module.ts
+++ projects/ui-framework/src/lib/navigation/breadcrumbs/breadcrumbs.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BreadcrumbsComponent } from './breadcrumbs.component';
 import { IconsModule } from '../../icons';
 import { TypographyModule } from '../../typography/typography.module';
+import { UtilsModule } from '../../utils/utils.module';
 
 @NgModule({
   declarations: [BreadcrumbsComponent],
-  imports: [CommonModule, IconsModule, TypographyModule],
+  imports: [CommonModule, IconsModule, TypographyModule, UtilsModule],
   exports: [BreadcrumbsComponent],
   providers: []
 })
