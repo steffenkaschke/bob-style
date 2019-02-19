d6dfcc5e67efd67642c38b44ac18307dca07f0b7
doroncy <doron.cy@gmail.com>
2019-02-19 10:32:42 +0200

    add utilsModule

--- projects/ui-framework/src/lib/navigation/breadcrumbs/breadcrumbs.module.ts
+++ projects/ui-framework/src/lib/navigation/breadcrumbs/breadcrumbs.module.ts
@@ -4,10 +4,11 @@ import { BreadcrumbsComponent } from './breadcrumbs.component';
 import { IconsModule } from '../../icons/icons.module';
 import { TypographyModule } from '../../typography/typography.module';
 import { UtilsService } from '../../utils/utils.service';
+import { UtilsModule } from '../../utils/utils.module';
 
 @NgModule({
   declarations: [BreadcrumbsComponent],
-  imports: [CommonModule, IconsModule, TypographyModule],
+  imports: [CommonModule, IconsModule, TypographyModule, UtilsModule],
   exports: [BreadcrumbsComponent],
   providers: [UtilsService]
 })
