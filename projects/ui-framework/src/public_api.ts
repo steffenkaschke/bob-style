474b3569381d73d4dbf0a5968dfea17bf64224b8
doroncy <doron.cy@gmail.com>
2019-03-04 18:02:54 +0200

    export missing interfasces

--- projects/ui-framework/src/public_api.ts
+++ projects/ui-framework/src/public_api.ts
@@ -36,7 +36,9 @@ export {
   InputAutoCompleteOptions
 } from './lib/form-elements/input/input.enum';
 // Split input + single select form field
-export { SplitInputSingleSelectModule } from './lib/form-elements/split-input-single-select/split-input-single-select.module';
+export {
+  SplitInputSingleSelectModule
+} from './lib/form-elements/split-input-single-select/split-input-single-select.module';
 // Lists
 export { SingleListModule } from './lib/form-elements/lists/single-list/single-list.module';
 export { SingleSelectModule } from './lib/form-elements/lists/single-select/single-select.module';
@@ -45,7 +47,11 @@ export { MultiSelectModule } from './lib/form-elements/lists/multi-select/multi-
 export { SelectGroupOption, SelectOption } from './lib/form-elements/lists/list.interface';
 // Breadcrumbs
 export { BreadcrumbsModule } from './lib/navigation/breadcrumbs/breadcrumbs.module';
-export { Breadcrumb } from './lib/navigation/breadcrumbs/breadcrumbs.interface';
+export {
+  Breadcrumb,
+  BreadcrumbNavButtons,
+  BreadcrumbNavButton
+} from './lib/navigation/breadcrumbs/breadcrumbs.interface';
 // Preloader
 export { MiniPreloaderModule } from './lib/misc/mini-preloader/mini-preloader.module';
 // Search
@@ -70,4 +76,8 @@ export { TabsModule } from './lib/navigation/tabs/tabs.module';
 export { Tab } from './lib/navigation/tabs/tabs.interface';
 // Quick Filters
 export { QuickFilterModule } from './lib/navigation/quick-filter/quick-filter.module';
-export { QuickFilterConfig, QuickFilterChangeEvent, QuickFilterBarChangeEvent } from './lib/navigation/quick-filter/quick-filter.interface';
+export {
+  QuickFilterConfig,
+  QuickFilterChangeEvent,
+  QuickFilterBarChangeEvent
+} from './lib/navigation/quick-filter/quick-filter.interface';
