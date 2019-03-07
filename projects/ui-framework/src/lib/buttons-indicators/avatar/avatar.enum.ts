30829392dd25420d468b120300df9ac2932daa30
Ishai <ishaiborovoy@gmail.com>
2019-03-07 10:59:11 +0200

    Allow to get number from AvatarSize (#278)

--- projects/ui-framework/src/lib/buttons-indicators/avatar/avatar.enum.ts
+++ projects/ui-framework/src/lib/buttons-indicators/avatar/avatar.enum.ts
@@ -4,3 +4,18 @@ export enum AvatarSize {
   medium  = 'medium',
   large  = 'large',
 }
+
+export namespace AvatarSize {
+  export function toNumber(avatarSize: AvatarSize): number {
+    switch (avatarSize) {
+      case AvatarSize.mini:
+        return 30;
+      case AvatarSize.small:
+        return 60;
+      case AvatarSize.medium:
+        return 90;
+      case AvatarSize.large:
+        return 120;
+    }
+  }
+}
