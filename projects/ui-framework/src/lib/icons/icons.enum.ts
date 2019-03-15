9369b3fef92352e2278ee7d2c5a7f023ba435aba
Omri Hecht <omrihecht@gmail.com>
2019-03-15 15:55:03 +0200

    added icons (#308)

--- projects/ui-framework/src/lib/icons/icons.enum.ts
+++ projects/ui-framework/src/lib/icons/icons.enum.ts
@@ -13,6 +13,15 @@ export enum IconColor {
 }
 
 export enum Icons {
+  home = 'sidebar_menu_home',
+  people = 'sidebar_menu_people_',
+  surveys = 'sidebar_menu_surveys',
+  tasks = 'sidebar_menu_tasks',
+  benefits = 'sidebar_menu_benefits',
+  reports = 'sidebar_menu_reports',
+  analytics = 'sidebar_menu_analytics',
+  harmonise = 'sidebar_menu_harmonise',
+  sidebar_settings = 'sidebar_menu_settings',
   calendar = 'calendar',
   toDos_link = 'todos_link',
   docs_link = 'docs_link',
