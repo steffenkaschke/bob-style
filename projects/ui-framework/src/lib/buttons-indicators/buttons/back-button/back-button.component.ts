1300c8fb644e69f49151273615199f381d4e440b
Pasha Golovin <pasha@galloween.net>
2019-03-18 10:46:44 +0200

    Pavel/back button (#314)
    * Simple Divider component
    
    * added some margin
    
    * reverted package-lock.json, added Divider component export to public_api.ts
    
    * added disabled state prop to back button
    
    * got rid of unnecessary span elements
    
    * icon light color on disabled
    
    * icon color uses enum, not string

--- projects/ui-framework/src/lib/buttons-indicators/buttons/back-button/back-button.component.ts
+++ projects/ui-framework/src/lib/buttons-indicators/buttons/back-button/back-button.component.ts
@@ -1,21 +1,19 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { ButtonSize, BackButtonType } from '../buttons.enum';
-import { Icons, IconSize } from '../../../icons/icons.enum';
+import { IconColor, Icons, IconSize } from '../../../icons/icons.enum';
 
 @Component({
   selector: 'b-back-button',
   template: `
     <b-button (clicked)="onClick($event)"
-              [type]="type"
-              [size]="size">
-      <span class="back-button-content-wrapper">
-        <b-icon icon="${Icons.back_arrow_link}"
-                size="${IconSize.small}">
-        </b-icon>
-        <span class="back-button-content">
-          <ng-content></ng-content>
-        </span>
-      </span>
+            [type]="type"
+            [size]="size"
+            [disabled]="disabled">
+      <b-icon icon="${Icons.back_arrow_link}"
+            size="${IconSize.small}"
+            [color]="disabled ? iconColor.light : color">
+      </b-icon>
+      <ng-content></ng-content>
     </b-button>
   `,
   styleUrls: ['./back-button.component.scss']
@@ -23,9 +21,14 @@ import { Icons, IconSize } from '../../../icons/icons.enum';
 export class BackButtonComponent {
 
   constructor() { }
+
   size: ButtonSize = ButtonSize.small;
   @Input() type?: BackButtonType = BackButtonType.secondary;
+  @Input() disabled = false;
+  @Input() color: IconColor = IconColor.dark;
   @Output() clicked: EventEmitter<void> = new EventEmitter<void>();
+
+  iconColor = IconColor;
 
   onClick($event) {
     this.clicked.emit($event);
