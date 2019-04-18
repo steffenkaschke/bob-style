3a3f52624d3ba1743e17fcf0bba572cfee46f8be
Dana Zelniker <dana.zelniker@hibob.com>
2019-04-18 14:23:27 +0300

    fix doc icon removal (#419)

--- projects/ui-framework/src/lib/icons/icons.enum.ts
+++ projects/ui-framework/src/lib/icons/icons.enum.ts
@@ -54,6 +54,7 @@ export enum Icons {
   employee_ref = 'employee_ref',
   docs_empty = 'docs_empty',
   baseline_info_icon = 'baseline_info_icon',
+  doc_icon = 'doc_icon',
   approve_badge = 'approve_badge',
   arrow_forward = 'arrow_forward',
   pending_badge = 'pending_badge'
