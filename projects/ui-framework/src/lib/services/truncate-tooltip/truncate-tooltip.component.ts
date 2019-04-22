5909714688bd427536a75cc8c131c13769dc0bbc
Pavel Golovin <pavelgolovin@Pavels-MBP.lan>
2019-04-22 12:11:56 +0300

    simplified component

--- projects/ui-framework/src/lib/services/truncate-tooltip/truncate-tooltip.component.ts
+++ projects/ui-framework/src/lib/services/truncate-tooltip/truncate-tooltip.component.ts
@@ -10,17 +10,17 @@ import {
 import { UtilsService } from '../utils/utils.service';
 import { Subscription } from 'rxjs';
 
-import { ElementData, Styles } from './truncate-tooltip.interface';
+import { Styles } from './truncate-tooltip.interface';
 
 @Component({
   selector: 'b-truncate-tooltip',
   template: `
     <span
       #textContainer
-      [matTooltip]="textElementData.text"
-      [matTooltipDisabled]="!textElementData.tooltipEnabled"
-      [class]="textElementData.class"
-      [ngStyle]="textElementData.style"
+      [matTooltip]="tooltipText"
+      [matTooltipDisabled]="!tooltipEnabled"
+      [ngClass]="textContainerClass"
+      [ngStyle]="textContainerStyle"
       matTooltipPosition="above"
       matTooltipClass="b-truncate-tooltip"
     >
@@ -39,82 +39,16 @@ export class TruncateTooltipComponent
 
   private textElement: HTMLElement;
   private resizeSubscription: Subscription;
-  textElementData: ElementData;
 
-  private getLineClampCSS(): Styles {
-    return {
-      maxHeight:
-        this.textElementData.fontSize *
-          this.textElementData.lineHeight *
-          this.maxLines +
-        'px',
-      webkitLineClamp: this.maxLines
-    };
-  }
-
-  private applyTextElementStyle() {
-    this.textElementData = {
-      ...this.textElementData,
-      class:
-        this.maxLines === 1 || !this.maxLines
-          ? 'truncate'
-          : this.maxLines > 1
-          ? 'line-clamp'
-          : null,
-      style: this.maxLines > 1 ? this.getLineClampCSS() : null
-    };
-  }
-
-  private getElementTextData(): void {
-    const computedStyle = getComputedStyle(this.textElement),
-      fontSize = parseFloat(computedStyle.fontSize),
-      lineHeight =
-        computedStyle.lineHeight.indexOf('px') !== -1
-          ? parseFloat(computedStyle.lineHeight) / fontSize
-          : computedStyle.lineHeight.indexOf('%') !== -1
-          ? parseFloat(computedStyle.lineHeight) / 100
-          : 1.2;
-
-    this.textElementData = {
-      ...this.textElementData,
-      fontSize: fontSize,
-      lineHeight: lineHeight,
-      text: this.textElement.innerText
-    };
-
-    // console.log(this.textContainer);
-    console.log('textElementData', this.textElementData);
-  }
-
-  private checkTooltipNecessity(): void {
-    this.textElementData = {
-      ...this.textElementData,
-
-      tooltipEnabled:
-        ((this.maxLines === 1 || !this.maxLines) &&
-          this.textContainer.nativeElement.scrollWidth >
-            this.textContainer.nativeElement.offsetWidth) ||
-        (this.maxLines > 0 &&
-          this.textContainer.nativeElement.scrollHeight >
-            this.textContainer.nativeElement.offsetHeight)
-          ? true
-          : false
-    };
-  }
-
-  private getDeepestNode(element: HTMLElement): HTMLElement {
-    while (element.children.length === 1) {
-      element = element.children[0] as HTMLElement;
-    }
-    return element;
-  }
+  textContainerClass: string;
+  textContainerStyle: Styles;
+  tooltipText: string;
+  tooltipEnabled = false;
 
   ngOnInit(): void {
     this.textElement = this.getDeepestNode(this.textContainer.nativeElement);
+    this.tooltipText = this.textElement.innerText;
 
-    console.log('textElement', this.textElement);
-
-    this.getElementTextData();
     this.applyTextElementStyle();
 
     this.resizeSubscription = this.utilsService
@@ -130,5 +64,50 @@ export class TruncateTooltipComponent
 
   ngOnDestroy(): void {
     this.resizeSubscription.unsubscribe();
+  }
+
+  private applyTextElementStyle(): void {
+    const computedStyle = getComputedStyle(this.textElement),
+      fontSize = parseFloat(computedStyle.fontSize),
+      lineHeight =
+        computedStyle.lineHeight.indexOf('px') !== -1
+          ? parseFloat(computedStyle.lineHeight) / fontSize
+          : computedStyle.lineHeight.indexOf('%') !== -1
+          ? parseFloat(computedStyle.lineHeight) / 100
+          : 1.2;
+
+    this.textContainerClass =
+      this.maxLines === 1 || !this.maxLines
+        ? 'truncate'
+        : this.maxLines > 1
+        ? 'line-clamp'
+        : null;
+
+    this.textContainerStyle =
+      this.maxLines > 1
+        ? {
+            maxHeight: fontSize * lineHeight * this.maxLines + 'px',
+            webkitLineClamp: this.maxLines
+          }
+        : null;
+  }
+
+  private checkTooltipNecessity(): void {
+    this.tooltipEnabled =
+      ((this.maxLines === 1 || !this.maxLines) &&
+        this.textContainer.nativeElement.scrollWidth >
+          this.textContainer.nativeElement.offsetWidth) ||
+      (this.maxLines > 0 &&
+        this.textContainer.nativeElement.scrollHeight >
+          this.textContainer.nativeElement.offsetHeight)
+        ? true
+        : false;
+  }
+
+  private getDeepestNode(element: HTMLElement): HTMLElement {
+    while (element.children.length === 1) {
+      element = element.children[0] as HTMLElement;
+    }
+    return element;
   }
 }
--- projects/ui-framework/src/lib/services/truncate-tooltip/truncate-tooltip.interface.ts
+++ projects/ui-framework/src/lib/services/truncate-tooltip/truncate-tooltip.interface.ts
@@ -1,12 +1,3 @@
 export interface Styles {
   [key: string]: string | number;
 }
-
-export interface ElementData {
-  text?: string;
-  fontSize?: number;
-  lineHeight?: number;
-  tooltipEnabled?: boolean;
-  class?: string;
-  style?: Styles;
-}
