dab1a9eda0922dd291e3375bfa5dd5ecbc6174a3
Pavel Golovin <pavelgolovin@Pavels-MBP.lan>
2019-04-29 09:25:31 +0300

    removed FunctionalUtils

--- projects/ui-framework/src/lib/services/utils/utils.module.ts
+++ projects/ui-framework/src/lib/services/utils/utils.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { UtilsService } from './utils.service';
 import { WindowRef } from './window-ref.service';
 import { DOMhelpers } from './dom-helpers.service';
-import { FunctionalUtils } from './functional.service';
 
 @NgModule({
   imports: [CommonModule],
-  providers: [UtilsService, WindowRef, DOMhelpers, FunctionalUtils]
+  providers: [UtilsService, WindowRef, DOMhelpers]
 })
 export class UtilsModule {}
