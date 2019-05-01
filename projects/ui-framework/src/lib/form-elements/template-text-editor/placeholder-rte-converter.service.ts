0c1afb617bf5d90450d11685b6e52b5294446d1a
yossi <yossitrx@gmail.com>
2019-05-01 11:53:44 +0300

    Template text editor | From RTE

--- projects/ui-framework/src/lib/form-elements/template-text-editor/placeholder-rte-converter.service.ts
+++ projects/ui-framework/src/lib/form-elements/template-text-editor/placeholder-rte-converter.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-import { find, get } from 'lodash';
+import { find } from 'lodash';
 import { Placeholder } from './placeholder-rte-converter';
+
 @Injectable()
 export class PlaceholderRteConverterService {
   constructor() {
+  }
+
+  public fromRte(rteInnerHtml: string, placeholders: Placeholder[]): string {
+    const elm: HTMLElement = document.createElement('div');
+    elm.innerHTML = rteInnerHtml + '';
+    Array.from(elm.querySelectorAll('[placeholder]')).forEach(e => {
+      const b: Text =
+        document.createTextNode('{{' +
+          this.matchPlaceholderToConvertedValueBtDisplyName(placeholders, e.innerHTML)
+          + '}}');
+      e.parentNode.replaceChild(b, e);
+    });
+    return elm.innerHTML.toString();
   }
 
   public toRte(contentToConvert: string, placeholders: Placeholder[]): string {
@@ -11,12 +25,17 @@ export class PlaceholderRteConverterService {
     return contentToConvert
       .replace(regex, (field: string, innerContent: string) => {
         // tslint:disable-next-line:max-line-length
-        return`<span placeholder="${innerContent}">${this.matchListOptionsToConvertedValue(placeholders, innerContent)}</span>`;
+        return`<span placeholder="${innerContent}">${this.matchPlaceholderToConvertedValueBtId(placeholders, innerContent)}</span>`;
       });
   }
 
-  private matchListOptionsToConvertedValue(placeholders: Placeholder[], id: string): string {
+  private matchPlaceholderToConvertedValueBtId(placeholders: Placeholder[], id: string): string {
     const placeholder = find(placeholders, p => p.id === id);
     return placeholder ? placeholder.displayName : id;
+  }
+
+  private matchPlaceholderToConvertedValueBtDisplyName(placeholders: Placeholder[], displayName: string): string {
+    const placeholder = find(placeholders, p => p.displayName === displayName);
+    return placeholder ? placeholder.id : displayName;
   }
 }
