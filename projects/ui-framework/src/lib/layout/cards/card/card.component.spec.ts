90fba32e031a97b9b6e992236c17b9f5781d1161
Pavel Golovin <pavelgolovin@Pavels-MacBook-Pro.local>
2019-05-01 11:13:13 +0300

    w.i.p.

--- projects/ui-framework/src/lib/layout/cards/card/card.component.spec.ts
+++ projects/ui-framework/src/lib/layout/cards/card/card.component.spec.ts
@@ -12,85 +12,85 @@ import { TypographyModule } from '../../../typography/typography.module';
 import { MockComponent } from 'ng-mocks';
 import { MenuComponent } from '../../../navigation/menu/menu.component';
 
-// xdescribe('CardComponent', () => {
-//   let fixture: ComponentFixture<CardComponent>;
-//   let component: CardComponent;
+fdescribe('CardComponent', () => {
+  let fixture: ComponentFixture<CardComponent>;
+  let component: CardComponent;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [MockComponent(MenuComponent), CardComponent],
-//       imports: [TypographyModule],
-//       schemas: [NO_ERRORS_SCHEMA]
-//     })
-//       .compileComponents()
-//       .then(() => {
-//         fixture = TestBed.createComponent(CardComponent);
-//         component = fixture.componentInstance;
-//       });
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MockComponent(MenuComponent), CardComponent],
+      imports: [TypographyModule],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CardComponent);
+        component = fixture.componentInstance;
+      });
+  }));
 
-//   describe('component', () => {
-//     it('should set input text inside <b-display-3> element', () => {
-//       component.text = 'hello';
-//       fixture.detectChanges();
-//       const bDisplay3Element = fixture.debugElement.query(
-//         By.css('b-display-3')
-//       );
-//       expect(bDisplay3Element.nativeElement.innerText).toEqual('hello');
-//     });
-//     it('should set input text inside <b-display-3> element and truncate text', () => {
-//       fixture.nativeElement.style.width = '200px';
-//       component.text = `Compensation update with a very long text that cuts off after 4 lines of text.
-//         And here is another very long text that should not be displayed at all.`;
-//       fixture.detectChanges();
-//       const bDisplay3Element = fixture.debugElement.query(
-//         By.css('b-display-3')
-//       );
-//       expect(bDisplay3Element.nativeElement.scrollHeight).toBeGreaterThan(
-//         bDisplay3Element.nativeElement.clientHeight
-//       );
-//     });
-//     it('should create menu element when menu configuration is passed', () => {
-//       component.menu = [{ label: '' }];
-//       fixture.detectChanges();
-//       const menuElement = fixture.debugElement.query(By.css('b-menu'));
-//       expect(menuElement).toBeTruthy();
-//     });
-//     it('should NOT create menu element when no menu configuration is passed', () => {
-//       const menuElement = fixture.debugElement.query(By.css('b-menu'));
-//       expect(menuElement).toBeFalsy();
-//     });
-//     it('should NOT create menu element when invalid menu configuration is passed', () => {
-//       component.menu = [];
-//       fixture.detectChanges();
-//       const menuElement = fixture.debugElement.query(By.css('b-menu'));
-//       expect(menuElement).toBeFalsy();
-//     });
-//   });
+  describe('component', () => {
+    it('should set input text inside <b-display-3> element', () => {
+      component.text = 'hello';
+      fixture.detectChanges();
+      const bDisplay3Element = fixture.debugElement.query(
+        By.css('b-display-3')
+      );
+      expect(bDisplay3Element.nativeElement.innerText).toEqual('hello');
+    });
+    it('should set input text inside <b-display-3> element and truncate text', () => {
+      fixture.nativeElement.style.width = '200px';
+      component.text = `Compensation update with a very long text that cuts off after 4 lines of text.
+        And here is another very long text that should not be displayed at all.`;
+      fixture.detectChanges();
+      const bDisplay3Element = fixture.debugElement.query(
+        By.css('b-display-3')
+      );
+      expect(bDisplay3Element.nativeElement.scrollHeight).toBeGreaterThan(
+        bDisplay3Element.nativeElement.clientHeight
+      );
+    });
+    it('should create menu element when menu configuration is passed', () => {
+      component.menu = [{ label: '' }];
+      fixture.detectChanges();
+      const menuElement = fixture.debugElement.query(By.css('b-menu'));
+      expect(menuElement).toBeTruthy();
+    });
+    it('should NOT create menu element when no menu configuration is passed', () => {
+      const menuElement = fixture.debugElement.query(By.css('b-menu'));
+      expect(menuElement).toBeFalsy();
+    });
+    it('should NOT create menu element when invalid menu configuration is passed', () => {
+      component.menu = [];
+      fixture.detectChanges();
+      const menuElement = fixture.debugElement.query(By.css('b-menu'));
+      expect(menuElement).toBeFalsy();
+    });
+  });
 
-//   describe('onMenuOpen', () => {
-//     it('should add focus-inside class on the host element', () => {
-//       component.menu = [{ label: '' }];
-//       fixture.detectChanges();
-//       const menuElement = fixture.debugElement.query(By.css('b-menu'));
-//       menuElement.componentInstance.openMenu.emit();
-//       fixture.detectChanges();
-//       expect(fixture.nativeElement.classList).toContain('focus-inside');
-//     });
-//   });
+  describe('onMenuOpen', () => {
+    it('should add focus-inside class on the host element', () => {
+      component.menu = [{ label: '' }];
+      fixture.detectChanges();
+      const menuElement = fixture.debugElement.query(By.css('b-menu'));
+      menuElement.componentInstance.openMenu.emit();
+      fixture.detectChanges();
+      expect(fixture.nativeElement.classList).toContain('focus-inside');
+    });
+  });
 
-//   describe('onMenuClose', () => {
-//     it('should remove focus-inside class from host element after timeout', fakeAsync(() => {
-//       component.menu = [{ label: '' }];
-//       fixture.detectChanges();
-//       const menuElement = fixture.debugElement.query(By.css('b-menu'));
-//       menuElement.componentInstance.openMenu.emit();
-//       menuElement.componentInstance.closeMenu.emit();
-//       fixture.detectChanges();
-//       expect(fixture.nativeElement.classList).toContain('focus-inside');
-//       tick(300);
-//       fixture.detectChanges();
-//       expect(fixture.nativeElement.classList).not.toContain('focus-inside');
-//     }));
-//   });
-// });
+  describe('onMenuClose', () => {
+    it('should remove focus-inside class from host element after timeout', fakeAsync(() => {
+      component.menu = [{ label: '' }];
+      fixture.detectChanges();
+      const menuElement = fixture.debugElement.query(By.css('b-menu'));
+      menuElement.componentInstance.openMenu.emit();
+      menuElement.componentInstance.closeMenu.emit();
+      fixture.detectChanges();
+      expect(fixture.nativeElement.classList).toContain('focus-inside');
+      tick(300);
+      fixture.detectChanges();
+      expect(fixture.nativeElement.classList).not.toContain('focus-inside');
+    }));
+  });
+});
