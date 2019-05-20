19ebb15c912d8ebd3dd71b17fe9acba85efcfc01
Pasha Golovin <psh.glvn@gmail.com>
2019-05-20 14:17:41 +0300

    updated test

--- projects/ui-framework/src/lib/navigation/tabs/tabs.component.spec.ts
+++ projects/ui-framework/src/lib/navigation/tabs/tabs.component.spec.ts
@@ -4,6 +4,7 @@ import { TabsComponent } from './tabs.component';
 import { MatTabsModule } from '@angular/material';
 import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsType } from './tabs.enum';
 
 describe('TabsComponent', () => {
   let component: TabsComponent;
@@ -11,15 +12,26 @@ describe('TabsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ MatTabsModule, BrowserAnimationsModule ],
-      declarations: [ TabsComponent ]
-    })
-    .compileComponents();
+      imports: [MatTabsModule, BrowserAnimationsModule],
+      declarations: [TabsComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TabsComponent);
     component = fixture.componentInstance;
+    component.tabs = [
+      {
+        label: 'tab 1'
+      },
+      {
+        label: 'tab 2'
+      },
+      {
+        label: 'tab 3'
+      }
+    ];
+    spyOn(component.selectChange, 'emit');
     fixture.detectChanges();
   });
 
@@ -29,19 +41,32 @@ describe('TabsComponent', () => {
 
   describe('tabs', () => {
     it('Should have 3 tabs', () => {
-      component.tabs = [{
-        label: 'tab 1',
-      },
-        {
-          label: 'tab 2',
-        },
-        {
-          label: 'tab 3',
-        }];
-      component.onSelectChange({index: 3, tab: null});
-      fixture.detectChanges();
       const progressEl = fixture.debugElement.query(By.css('.mat-tab-header'));
       expect(progressEl.childNodes.length).toEqual(3);
+    });
+
+    it('Should be able to set type (style)', () => {
+      component.type = TabsType.secondary;
+      fixture.detectChanges();
+
+      const tabsGroupEl = fixture.debugElement.query(By.css('.mat-tab-group'))
+        .nativeElement;
+      expect(tabsGroupEl.classList).not.toContain('tabs-primary');
+      expect(tabsGroupEl.classList).toContain('tabs-secondary');
+    });
+
+    it('Should be able to set selected tab via selectedIndex', () => {
+      component.selectedIndex = 2;
+      fixture.detectChanges();
+      const labelEl = fixture.debugElement.query(
+        By.css('.mat-tab-label:nth-child(3)')
+      ).nativeElement;
+      expect(labelEl.classList).toContain('mat-tab-label-active');
+    });
+
+    it('Should output selectChange event', () => {
+      component.selectedIndex = 1;
+      expect(component.selectChange.emit).not.toHaveBeenCalled();
     });
   });
 });
