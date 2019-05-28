d83be3185eaf30a64750019229eb68202a0b049d
Pasha Golovin <psh.glvn@gmail.com>
2019-05-28 15:39:48 +0300

    removed console.log

--- projects/ui-framework/src/lib/icons/icon.service.ts
+++ projects/ui-framework/src/lib/icons/icon.service.ts
@@ -8,16 +8,25 @@ const ICONS_CDN = 'https://images.hibob.com/icons/';
 
 @Injectable()
 export class IconService {
-  constructor(private iconRegistry: MatIconRegistry,
-              private sanitizer: DomSanitizer) {
-  }
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {}
 
   public initIcon(icon: Icons): void {
-    this.iconRegistry.getNamedSvgIcon(icon).pipe(first()).subscribe(() => {
-    }, () => {
-      console.log('error', icon);
-      this.iconRegistry.addSvgIcon(icon,
-        this.sanitizer.bypassSecurityTrustResourceUrl(`${ICONS_CDN}${icon}.svg`));
-    });
+    this.iconRegistry
+      .getNamedSvgIcon(icon)
+      .pipe(first())
+      .subscribe(
+        () => {},
+        () => {
+          this.iconRegistry.addSvgIcon(
+            icon,
+            this.sanitizer.bypassSecurityTrustResourceUrl(
+              `${ICONS_CDN}${icon}.svg`
+            )
+          );
+        }
+      );
   }
 }
