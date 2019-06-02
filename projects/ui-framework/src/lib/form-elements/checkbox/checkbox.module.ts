9dd0253ffffc9b90c11656dd7517db32d7f35a0b
Pasha Golovin <psh.glvn@gmail.com>
2019-06-02 20:10:04 +0300

    cleanup

--- projects/ui-framework/src/lib/form-elements/checkbox/checkbox.module.ts
+++ projects/ui-framework/src/lib/form-elements/checkbox/checkbox.module.ts
@@ -3,16 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CheckboxComponent } from './checkbox.component';
 import { MatPseudoCheckboxModule } from '@angular/material/';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CheckboxComponent],
-  imports: [
-    CommonModule,
-    MatPseudoCheckboxModule,
-    MatCheckboxModule,
-    FormsModule
-  ],
+  imports: [CommonModule, MatPseudoCheckboxModule, MatCheckboxModule],
   exports: [CheckboxComponent]
 })
 export class CheckboxModule {}
