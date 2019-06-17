3acf1e5f3caccf2c994cb047ae0f653801910130
Pasha Golovin <psh.glvn@gmail.com>
2019-06-18 00:10:15 +0300

    more

--- projects/ui-framework/src/lib/popups/lightbox/lightbox-example.module.ts
+++ projects/ui-framework/src/lib/popups/lightbox/lightbox-example.module.ts
@@ -21,15 +21,15 @@ import { LightboxConfig } from './lightbox.interface';
     >
     &nbsp;
     <b-button id="b3" (click)="showLightbox(configs[4])">Show link</b-button>
-    <br /><br />
-    <b-button id="b4" (click)="showLightboxAll()">Show all</b-button>
+    <!-- <br /><br />
+    <b-button id="b4" (click)="showLightboxAll()">Show all</b-button> -->
   `,
   providers: []
 })
 export class LightboxExampleComponent implements OnDestroy {
   constructor(private lightboxService: LightboxService) {}
 
-  imgDelay = ''; // 'http://deelay.me/1000/';
+  imgDelay = 'http://deelay.me/1000/';
 
   configs = [
     {
