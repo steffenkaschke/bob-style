3032bae9936f67cdb78c66470decd46841f92666
Pasha Golovin <psh.glvn@gmail.com>
2019-06-23 21:48:31 +0300

    random updates

--- projects/ui-framework/src/lib/buttons-indicators/buttons/square/square.component.ts
+++ projects/ui-framework/src/lib/buttons-indicators/buttons/square/square.component.ts
@@ -5,14 +5,18 @@ import { IconColor, Icons, IconSize } from '../../../icons/icons.enum';
 @Component({
   selector: 'b-square-button',
   template: `
-    <button mat-button
-            [disableRipple]="true"
-            class="{{type}}"
-            [ngClass]="{'disabled': disabled}"
-            (click)="onClick($event)">
-      <b-icon [icon]="icon"
-              [size]="iconSize.large"
-              [color]="disabled ? iconColor.light : color">
+    <button
+      mat-button
+      [disableRipple]="true"
+      class="{{ type }}"
+      [ngClass]="{ disabled: disabled }"
+      (click)="onClick($event)"
+    >
+      <b-icon
+        [icon]="icon"
+        [size]="iconSize"
+        [color]="disabled ? iconColor.light : color"
+      >
       </b-icon>
     </button>
   `,
@@ -21,15 +25,14 @@ import { IconColor, Icons, IconSize } from '../../../icons/icons.enum';
 export class SquareButtonComponent {
   @Input() type?: ButtonType = ButtonType.primary;
   @Input() icon: Icons;
+  @Input() iconSize: IconSize = IconSize.large;
   @Input() color: IconColor = IconColor.dark;
   @Input() disabled = false;
   @Output() clicked: EventEmitter<void> = new EventEmitter<void>();
 
-  iconSize = IconSize;
   iconColor = IconColor;
 
-  constructor() {
-  }
+  constructor() {}
 
   onClick($event) {
     this.clicked.emit($event);
