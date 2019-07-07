681a5e3b3ec7e5268f908e4b83b1b45381031425
Pasha Golovin <psh.glvn@gmail.com>
2019-07-07 13:11:56 +0300

    getWindowClickEvent, getWindowKeydownEvent in utils.service

--- projects/ui-framework/src/lib/popups/panel/panel.module.ts
+++ projects/ui-framework/src/lib/popups/panel/panel.module.ts
@@ -3,21 +3,12 @@ import { CommonModule } from '@angular/common';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { PanelComponent } from './panel.component';
 import { PanelPositionService } from './panel-position-service/panel-position.service';
+import { UtilsService } from '../../services/utils/utils.service';
 
 @NgModule({
-  declarations: [
-    PanelComponent,
-  ],
-  imports: [
-    CommonModule,
-    OverlayModule,
-  ],
-  exports: [
-    PanelComponent,
-  ],
-  providers: [
-    PanelPositionService,
-  ],
+  declarations: [PanelComponent],
+  imports: [CommonModule, OverlayModule],
+  exports: [PanelComponent],
+  providers: [PanelPositionService, UtilsService]
 })
-export class PanelModule {
-}
+export class PanelModule {}
