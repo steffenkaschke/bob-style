cd01bdfa52258de9be030b91c52b031c22f2c227
Omri Hecht <omrihecht@gmail.com>
2019-07-08 20:24:14 +0300

    cards interface update (#738)
    * bump version [skip ci]
    
    * card interface

--- projects/ui-framework/src/lib/cards/cards.interface.ts
+++ projects/ui-framework/src/lib/cards/cards.interface.ts
@@ -3,6 +3,7 @@ import { MenuItem } from '../navigation/menu/menu.interface';
 export interface CardContent {
   text?: string;
   subText?: string;
+  id?: string | number;
 }
 
 export interface CardData {
@@ -14,10 +15,11 @@ export interface CardEmployee {
   imageSource: string;
   title: string;
   subtitle: string;
+  id?: string;
 }
 
 export interface AddCardData {
   title: string;
   subtitle?: string;
-  action: (...args: any[]) => void;
+  action?: (...args: any[]) => void;
 }
