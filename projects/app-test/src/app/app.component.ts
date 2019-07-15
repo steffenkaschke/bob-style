1ed1f647af4d68c92f0416bd652fd5a5ca03535c
Pasha Golovin <psh.glvn@gmail.com>
2019-07-15 10:47:47 +0300

    outsideZone operator + misc adjustments

--- projects/app-test/src/app/app.component.ts
+++ projects/app-test/src/app/app.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UtilsService } from '../../../ui-framework/src/lib/services/utils/utils.service';
+import { outsideZone } from '../../../ui-framework/src/lib/services/utils/rxjs.operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  constructor(private utilsService: UtilsService, private zone: NgZone) {}
 
   editorValue = 'some text';
+  scrollSubscription: Subscription;
+  resizeSubscription: Subscription;
 
   updateValue(event) {
     console.log(event);
     this.editorValue = event;
   }
+
+  ngOnInit(): void {
+    this.scrollSubscription = this.utilsService
+      .getScrollEvent()
+      .pipe(outsideZone(this.zone))
+      .subscribe(scrollPos => {
+        console.log(scrollPos);
+      });
+    this.resizeSubscription = this.utilsService
+      .getResizeEvent()
+      .pipe(outsideZone(this.zone))
+      .subscribe(scrollPos => {
+        console.log('window resized');
+      });
+  }
+
+  ngOnDestroy(): void {}
 }
