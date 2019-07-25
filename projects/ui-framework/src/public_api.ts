ed72e146a00ece2421141c47f5129be2f7ad6f0a
Pasha Golovin <psh.glvn@gmail.com>
2019-07-25 09:28:46 +0300

    added to public api

--- projects/ui-framework/src/public_api.ts
+++ projects/ui-framework/src/public_api.ts
@@ -315,6 +315,7 @@ export { AlertType } from './lib/popups/alert/alert.enum';
 export { LightboxModule } from './lib/popups/lightbox/lightbox.module';
 export { LightboxService } from './lib/popups/lightbox/lightbox.service';
 export { LightboxComponent } from './lib/popups/lightbox/lightbox.component';
+export { VideoEmbedComponent } from './lib/popups/lightbox/video-embed/video-embed.component';
 export { LightboxConfig, LightboxData } from './lib/popups/lightbox/lightbox.interface';
 
 
