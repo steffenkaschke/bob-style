eeb3b2e5df84cfac9e5f63eaea2fe8b10c626534
Ben Baler <ben.baler@hibob.io>
2019-07-30 16:41:58 +0300

    side menu fixes

--- projects/ui-framework/src/lib/navigation/side-menu/side-menu.component.ts
+++ projects/ui-framework/src/lib/navigation/side-menu/side-menu.component.ts
@@ -1,29 +1,17 @@
-import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SideMenuOption } from './side-menu-option/side-menu-option.interface';
-import head from 'lodash/head';
-import size from 'lodash/size';
-import isNull from 'lodash/isNull';
-import find from 'lodash/find';
 
 @Component({
   selector: 'b-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss']
 })
-export class SideMenuComponent implements OnChanges {
+export class SideMenuComponent {
   @Input() options: SideMenuOption[];
   @Input() selectedId: number = null;
   @Output() selectOption: EventEmitter<number> = new EventEmitter<number>();
 
   constructor() {
-  }
-
-  ngOnChanges() {
-    if (size(this.options)) {
-      if (isNull(this.selectedId) || !find(this.options, { id: this.selectedId })) {
-        this.onSelectOption(head(this.options).id);
-      }
-    }
   }
 
   onSelectOption(id: number): void {
