3fdc3e9bb4923348f3c8c655f108c7fd6351c3aa
Danny <danny@shoppimon.com>
2019-08-19 13:11:07 +0300

    add delete event

--- projects/ui-framework/src/lib/form-elements/lists/chain-select/chain-select.component.ts
+++ projects/ui-framework/src/lib/form-elements/lists/chain-select/chain-select.component.ts
@@ -1,52 +1,43 @@
 import { Component, EventEmitter, Input, OnInit, Output, Type, ContentChild } from '@angular/core';
-import { isEmpty, clone } from 'lodash';
-import { ChainLink, SelectComponentConfig } from './chain-select.interface';
 import { Icons, IconSize } from '../../../icons/icons.enum';
 import { ButtonSize, ButtonType } from '../../../buttons/buttons.enum';
-import { ListChange } from '../list-change/list-change';
 import { ChainSelectDirective } from './chain-select.directive';
+import { ChainSelectEvent } from './chain-select.interface';
+import { ChainSelectEventEnum } from './chain-select.enum';
 
 @Component({
   selector: 'b-chain-select',
   templateUrl: './chain-select.component.html',
   styleUrls: ['./chain-select.component.scss']
 })
-export class ChainSelectComponent implements OnInit {
+export class ChainSelectComponent {
   @Input() actionLabel: string;
-  @Input() selectedItemList: [];
-  @Output() selectChange: EventEmitter<{ $event: any, index: number }> =
-    new EventEmitter<{ $event: any, index: number }>();
+  @Input() selectedItemList: any[] = [];
+  @Output() selectChange: EventEmitter<ChainSelectEvent> =
+    new EventEmitter<ChainSelectEvent>();
 
   readonly icons = Icons;
   readonly iconSize = IconSize;
   readonly buttonType = ButtonType;
   readonly buttonSize = ButtonSize;
-  public chainLinkList: ChainLink[];
-  public state: ListChange[];
 
   @ContentChild(ChainSelectDirective, { static: true }) contentChild !: ChainSelectDirective;
 
   constructor() {}
 
-  ngOnInit() {
-     this.chainLinkList = [];
-  }
-
   public addChainLink() {
-    this.chainLinkList.push(this.createEmptyChainLink(this.chainLinkList.length));
+    this.selectedItemList.push(null);
   }
 
   public removeChainLink(index: number) {
-    this.chainLinkList.splice(index, 1);
-    this.state.splice(index, 1);
-    //this.selectChange.emit(this.state);
-  }
-
-  private createEmptyChainLink(index: number): ChainLink {
-    return null;
+    this.selectedItemList.splice(index, 1);
+    this.selectChange.emit({
+      index,
+      event: ChainSelectEventEnum.delete,
+    });
   }
 
   public handleChange($event, index) {
-    this.selectChange.emit({ $event, index });
+    this.selectChange.emit({ index, event: $event });
   }
 }
