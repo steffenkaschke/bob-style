62d041b2c3913987fc4774890be0419fe790e936
Harel Levy <harellevy@gmail.com>
2019-08-22 12:37:53 +0300

    - some tests

--- projects/ui-framework/src/lib/chips/emoji-chip-list/emoji-chip-list.component.spec.ts
+++ projects/ui-framework/src/lib/chips/emoji-chip-list/emoji-chip-list.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmojiChipListComponent } from './emoji-chip-list.component';
+import {MockPipe} from 'ng-mocks';
+import {EmojiFromCodePipe} from './emoji-from-code.pipe';
 
-describe('EmojiChipListComponent', () => {
+fdescribe('EmojiChipListComponent', () => {
   let component: EmojiChipListComponent;
   let fixture: ComponentFixture<EmojiChipListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EmojiChipListComponent ]
+      declarations: [ EmojiChipListComponent,
+        MockPipe(EmojiFromCodePipe, (val) => `${val}`)
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +25,17 @@ describe('EmojiChipListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should call formatterFunction if exists and return value if it doesnt', () => {
+    expect(component.valueFormatterFn(42)).toEqual(42);
+    component.valueFormatter = (val) => {
+      return val * 2;
+    };
+    expect(component.valueFormatterFn(42)).toEqual(84);
+  });
+  it('should call chipClick emitter when function chipClick called', () => {
+    const chipClickedSpy = spyOn(component.chipClicked, 'emit');
+    component.chipClick({emoji: '1F45F', number: 245});
+    expect(chipClickedSpy).toHaveBeenCalledWith({emoji: '1F45F', number: 245});
   });
 });
--- projects/ui-framework/src/lib/chips/emoji-chip-list/emoji-from-code.pipe.spec.ts
+++ projects/ui-framework/src/lib/chips/emoji-chip-list/emoji-from-code.pipe.spec.ts
@@ -1,8 +1,15 @@
 import { EmojiFromCodePipe } from './emoji-from-code.pipe';
 
-describe('EmojiFromCodePipe', () => {
+fdescribe('EmojiFromCodePipe', () => {
   it('create an instance', () => {
     const pipe = new EmojiFromCodePipe();
     expect(pipe).toBeTruthy();
+  });
+  it('should transform method convert emoji code to emoji UTF8 string', () => {
+    const pipe = new EmojiFromCodePipe();
+    expect(pipe.transform('1F45B')).toEqual('👛');
+    expect(pipe.transform('1F45C')).toEqual('👜');
+    expect(pipe.transform('1F412')).toEqual('🐒');
+    expect(pipe.transform('1F414')).toEqual('🐔');
   });
 });
