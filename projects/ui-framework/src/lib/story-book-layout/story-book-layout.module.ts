4b5519cc0e6ccc965c6119fe994a3527a25cde32
Pasha Golovin <psh.glvn@gmail.com>
2019-10-21 13:21:14 +0300

    added mat tooltip default options

--- projects/ui-framework/src/lib/story-book-layout/story-book-layout.module.ts
+++ projects/ui-framework/src/lib/story-book-layout/story-book-layout.module.ts
@@ -5,11 +5,15 @@ import { TypographyModule } from '../typography/typography.module';
 import 'zone.js/dist/zone-patch-rxjs';
 import { StatsModule } from '../services/util-components/stats.module';
 import { UtilsService } from '../services/utils/utils.service';
+import { MAT_TOOLTIP_DEFAULT_OPTIONS } from '@angular/material';
 
 @NgModule({
   declarations: [StoryBookLayoutComponent],
   imports: [CommonModule, TypographyModule, StatsModule],
   exports: [StoryBookLayoutComponent],
-  providers: [UtilsService]
+  providers: [UtilsService, {
+    provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: {
+      showDelay: 300,
+      position: 'above'} }]
 })
 export class StoryBookLayoutModule {}
