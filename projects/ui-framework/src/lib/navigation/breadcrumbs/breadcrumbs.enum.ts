96692b99fffa45c7ec162e73175117f3485cc1c9
Itay Oded <itayoded1@gmail.com>
2019-11-12 11:30:33 +0200

    add toggle strategy to breadcrumbs (#1093)

--- projects/ui-framework/src/lib/navigation/breadcrumbs/breadcrumbs.enum.ts
+++ projects/ui-framework/src/lib/navigation/breadcrumbs/breadcrumbs.enum.ts
@@ -2,3 +2,10 @@ export enum BreadcrumbsType {
   primary = 'primary',
   secondary = 'secondary'
 }
+
+export enum BreadcrumbsToggleStrategy {
+  // will display labels according to the active step
+  auto = 'auto',
+  // will display labels always
+  alwaysOpen = 'alwaysOpen'
+}
