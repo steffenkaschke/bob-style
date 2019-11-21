ef394feda0cede85c32771414a238dcf58af06a0
Danny Berger <dannybmail@gmail.com>
2019-11-21 15:48:50 +0200

    move basic list to lists

--- projects/ui-framework/src/lib/lists/basic-list/basic-list.module.ts
+++ projects/ui-framework/src/lib/lists/basic-list/basic-list.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicListComponent } from './basic-list.component';
-import { IconsModule } from '../../../icons/icons.module';
+import { IconsModule } from '../../icons/icons.module';
 import { BasicListActionDirective } from './basic-list-action/basic-list-action.directive';
 
 @NgModule({
