1cc54dab4c0b721658eff8e2334ed5eadc92e955
Pasha Golovin <psh.glvn@gmail.com>
2019-11-24 11:22:01 +0200

    remove BrowserAnimationsModule from menu module

--- projects/ui-framework/src/lib/navigation/menu/menu.component.spec.ts
+++ projects/ui-framework/src/lib/navigation/menu/menu.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { By } from '@angular/platform-browser';
@@ -15,36 +15,30 @@ describe('MenuComponent', () => {
       {
         label: 'button 0',
         key: 'button.zero',
-        action: ($event) => console.log('button 0', $event),
+        action: $event => console.log('button 0', $event),
       },
       {
         label: 'button 1',
-        action: ($event) => console.log('button 1', $event),
+        action: $event => console.log('button 1', $event),
       },
       {
         label: 'button 2',
         children: [
           {
             label: 'button 3',
-            action: ($event) => console.log('button 3', $event),
+            action: $event => console.log('button 3', $event),
           },
           {
             label: 'button 4',
-            action: ($event) => console.log('button 4', $event),
+            action: $event => console.log('button 4', $event),
           },
         ],
       },
     ];
 
     TestBed.configureTestingModule({
-      declarations: [
-        MenuComponent,
-      ],
-      imports: [
-        NoopAnimationsModule,
-        MatMenuModule,
-        BrowserAnimationsModule,
-      ],
+      declarations: [MenuComponent],
+      imports: [NoopAnimationsModule, MatMenuModule],
     })
       .compileComponents()
       .then(() => {
@@ -58,7 +52,9 @@ describe('MenuComponent', () => {
 
   describe('menu', () => {
     const openMenu = () => {
-      const menuTrigger = fixture.debugElement.query(By.css('.menu-trigger-wrapper'));
+      const menuTrigger = fixture.debugElement.query(
+        By.css('.menu-trigger-wrapper')
+      );
       menuTrigger.triggerEventHandler('click', null);
     };
     it('should open menu on menuTrigger click', () => {
@@ -80,14 +76,18 @@ describe('MenuComponent', () => {
       component.menu = menuMock;
       fixture.detectChanges();
       openMenu();
-      const menuOptions = fixture.debugElement.queryAll(By.css('.mat-menu-item'));
+      const menuOptions = fixture.debugElement.queryAll(
+        By.css('.mat-menu-item')
+      );
       expect(menuOptions.length).toEqual(3);
     });
     it('should display label in option', () => {
       component.menu = menuMock;
       fixture.detectChanges();
       openMenu();
-      const menuOptions = fixture.debugElement.queryAll(By.css('.mat-menu-item span'));
+      const menuOptions = fixture.debugElement.queryAll(
+        By.css('.mat-menu-item span')
+      );
       expect(menuOptions[0].nativeElement.innerText).toEqual('button 0');
       expect(menuOptions[1].nativeElement.innerText).toEqual('button 1');
       expect(menuOptions[2].nativeElement.innerText).toEqual('button 2');
@@ -97,7 +97,9 @@ describe('MenuComponent', () => {
       component.menu = menuMock;
       fixture.detectChanges();
       openMenu();
-      const menuOption = fixture.debugElement.queryAll(By.css('.mat-menu-item'))[1];
+      const menuOption = fixture.debugElement.queryAll(
+        By.css('.mat-menu-item')
+      )[1];
       menuOption.triggerEventHandler('click', null);
       expect(menuMock[1].action).toHaveBeenCalledTimes(1);
     });
@@ -106,28 +108,34 @@ describe('MenuComponent', () => {
       component.menu = menuMock;
       fixture.detectChanges();
       openMenu();
-      const menuOption = fixture.debugElement.queryAll(By.css('.mat-menu-item'))[1];
+      const menuOption = fixture.debugElement.queryAll(
+        By.css('.mat-menu-item')
+      )[1];
       menuOption.triggerEventHandler('click', null);
       expect(component.actionClick.emit).toHaveBeenCalledWith(menuMock[1]);
     });
     it('should disable option when config has disabled=true', () => {
       menuMock[1] = {
         label: 'button 2',
-        action: ($event) => console.log('button 2', $event),
+        action: $event => console.log('button 2', $event),
         disabled: true,
       };
       spyOn(menuMock[1], 'action');
       component.menu = menuMock;
       fixture.detectChanges();
       openMenu();
-      const menuOption = fixture.debugElement.queryAll(By.css('.mat-menu-item'))[1];
+      const menuOption = fixture.debugElement.queryAll(
+        By.css('.mat-menu-item')
+      )[1];
       expect(menuOption.nativeElement.classList).toContain('disabled');
     });
     it('should add class from config key', () => {
       component.menu = menuMock;
       fixture.detectChanges();
       openMenu();
-      const menuOption = fixture.debugElement.queryAll(By.css('.mat-menu-item'))[0];
+      const menuOption = fixture.debugElement.queryAll(
+        By.css('.mat-menu-item')
+      )[0];
       expect(menuOption.nativeElement.classList).toContain('button.zero');
     });
   });
@@ -136,29 +144,39 @@ describe('MenuComponent', () => {
     it('should update menuDir with before', () => {
       component.ngOnChanges({
         openLeft: {
-          previousValue: undefined, currentValue: true, firstChange: true, isFirstChange: () => true,
-        }
+          previousValue: undefined,
+          currentValue: true,
+          firstChange: true,
+          isFirstChange: () => true,
+        },
       });
       expect(component.menuDir).toEqual('before');
     });
     it('should update menuDir with after', () => {
       component.ngOnChanges({
         openLeft: {
-          previousValue: true, currentValue: false, firstChange: false, isFirstChange: () => false,
-        }
+          previousValue: true,
+          currentValue: false,
+          firstChange: false,
+          isFirstChange: () => false,
+        },
       });
       expect(component.menuDir).toEqual('after');
     });
     it('should disable menu trigger', () => {
       component.ngOnChanges({
         disabled: {
-          previousValue: undefined, currentValue: true, firstChange: true, isFirstChange: () => true,
-        }
+          previousValue: undefined,
+          currentValue: true,
+          firstChange: true,
+          isFirstChange: () => true,
+        },
       });
       fixture.detectChanges();
-      const menuTrigger = fixture.debugElement.query(By.css('.menu-trigger-wrapper'));
+      const menuTrigger = fixture.debugElement.query(
+        By.css('.menu-trigger-wrapper')
+      );
       expect(menuTrigger.nativeElement.classList).toContain('disabled');
     });
   });
-
 });
--- projects/ui-framework/src/lib/navigation/menu/menu.module.ts
+++ projects/ui-framework/src/lib/navigation/menu/menu.module.ts
@@ -1,19 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [MenuComponent],
-  imports: [
-    CommonModule,
-    MatMenuModule,
-    BrowserAnimationsModule,
-  ],
-  exports: [
-    MenuComponent,
-  ],
+  imports: [CommonModule, MatMenuModule],
+  exports: [MenuComponent],
 })
-export class MenuModule {
-}
+export class MenuModule {}
