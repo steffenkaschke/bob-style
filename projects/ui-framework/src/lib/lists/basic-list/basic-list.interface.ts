e2ddd7eba9c061e80d1a9aaca647ef3bbd6ca958
Danny Berger <dannybmail@gmail.com>
2019-11-24 15:51:05 +0200

    make interface generic

--- projects/ui-framework/src/lib/lists/basic-list/basic-list.interface.ts
+++ projects/ui-framework/src/lib/lists/basic-list/basic-list.interface.ts
@@ -1,8 +1,7 @@
 import { Icons } from '../../icons/icons.enum';
-import { MenuItem } from '../../navigation/menu/menu.interface';
 
 export interface BasicListItem {
   label: string;
   icon: Icons;
-  menu?: MenuItem[];
+  [key: string]: any;
 }
