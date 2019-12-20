a979062820e5a0ae02334dc8ed1a4a6787db1da9
Pasha Golovin <psh.glvn@gmail.com>
2019-12-20 18:42:32 +0200

    put back

--- projects/ui-framework/src/lib/form-elements/date-picker/date-range-picker/date-range-picker.component.spec.ts
+++ projects/ui-framework/src/lib/form-elements/date-picker/date-range-picker/date-range-picker.component.spec.ts
@@ -97,7 +97,7 @@ describe('DateRangePickerComponent', () => {
   describe('OnInit', () => {
     it('should not display clear buttons when inputs are empty', () => {
       expect(iconElems.length).toEqual(0);
-      // expect(false).toEqual(true);
+      expect(false).toEqual(true);
     });
   });
 
