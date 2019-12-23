4f57f0e1dca79ae3fbd60ae94207c0fb9e535cd2
Pasha Golovin <psh.glvn@gmail.com>
2019-12-23 09:21:51 +0200

    same

--- projects/ui-framework/src/lib/lists/list-service/list-model.service.ts
+++ projects/ui-framework/src/lib/lists/list-service/list-model.service.ts
@@ -236,6 +236,4 @@ export class ListModelService {
   totalOptionsCount(options: SelectGroupOption[]): number {
     return arrayFlatten(options.map(group => group.options)).length;
   }
-
-  // bye why not?
 }
