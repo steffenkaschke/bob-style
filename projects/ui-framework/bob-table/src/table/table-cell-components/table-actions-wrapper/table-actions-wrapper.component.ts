edf75861201f4bb2a891f03a1163131dc4092d3d
Ishai Borovoy <ishai.borovoy@hibob.com>
2019-12-31 13:03:03 +0200

    Adding the icon to the actions wrapper

--- projects/ui-framework/bob-table/src/table/table-cell-components/table-actions-wrapper/table-actions-wrapper.component.ts
+++ projects/ui-framework/bob-table/src/table/table-cell-components/table-actions-wrapper/table-actions-wrapper.component.ts
@@ -7,8 +7,9 @@ import { ButtonSize, ButtonType, IconColor, Icons, MenuItem } from 'bob-style';
   styleUrls: ['./table-actions-wrapper.component.scss']
 })
 export class TableActionsWrapperComponent implements OnInit {
-  @Input() public menuItems: MenuItem[];
-  @Input() public primary = true;
+  @Input() menuItems: MenuItem[];
+  @Input() primary = true;
+  @Input() icon: Icons = null;
   readonly buttonSize = ButtonSize;
   readonly buttonType = ButtonType
 
