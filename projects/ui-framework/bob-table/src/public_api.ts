89a05cd981d5d64215809b5ebe8399141c049178
itay <itayoded1@gmail.com>
2020-01-01 14:57:09 +0200

    expose table tree types

--- projects/ui-framework/bob-table/src/public_api.ts
+++ projects/ui-framework/bob-table/src/public_api.ts
@@ -7,6 +7,8 @@
 export * from './table/table/table.interface';
 export * from './table/table/table.interface';
 export * from './table/table/table.enum';
+export * from './table/table/extensions/tree.directive';
+export * from './table/table/extensions/tree.config';
 export { TableComponent } from './table/table/table.component';
 export { AvatarCellComponent } from './table/table-cell-components/avatar-cell/avatar.component';
 export { ActionsCellComponent } from './table/table-cell-components/actions-cell/actions-cell.component';
