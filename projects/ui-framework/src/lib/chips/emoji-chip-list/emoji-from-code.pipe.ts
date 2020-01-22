493b17cbaf750d013329e7282d69066d89171496
Harel Levy <harellevy@gmail.com>
2020-01-22 16:07:11 +0200

    - emoji support multi emoji code (i.e 1f469-200d-2695)

--- projects/ui-framework/src/lib/chips/emoji-chip-list/emoji-from-code.pipe.ts
+++ projects/ui-framework/src/lib/chips/emoji-chip-list/emoji-from-code.pipe.ts
@@ -4,9 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'emojiFromCode'
 })
 export class EmojiFromCodePipe implements PipeTransform {
-
   transform(value: string, args?: any): any {
-    return String.fromCodePoint( Number('0x' + value) );
+    return String.fromCodePoint(...value.split('-').map(u => Number(`0x${u}`)));
   }
-
 }
