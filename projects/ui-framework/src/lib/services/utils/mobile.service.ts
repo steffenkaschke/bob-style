b02b50d28c60605e8501e32b5b86ef20cb7ceb92
Ori Danus <oridanus@gmail.com>
2020-02-25 10:41:32 +0200

    fix to ignore case

--- projects/ui-framework/src/lib/services/utils/mobile.service.ts
+++ projects/ui-framework/src/lib/services/utils/mobile.service.ts
@@ -88,7 +88,7 @@ export class MobileService {
 
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
     // @ts-ignore
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    if (/ipad|iphone|ipod/i.test(userAgent) && !window.MSStream) {
       return MobileOS.iOS;
     }
 
