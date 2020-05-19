9b6e0c17bf161dac1f8b5096266584daad2ba4bb
Doron Cyngiser <doron.cy@gmail.com>
2020-05-19 15:53:46 +0300

    moved the translation to the storybook layout init step (#1526)

--- projects/ui-framework/src/lib/story-book-layout/story-book-layout.module.ts
+++ projects/ui-framework/src/lib/story-book-layout/story-book-layout.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoryBookLayoutComponent } from './story-book-layout.component';
 import { TypographyModule } from '../typography/typography.module';
@@ -6,18 +6,44 @@ import { StatsModule } from '../services/util-components/stats.module';
 import { UtilsService } from '../services/utils/utils.service';
 import 'zone.js/dist/zone-patch-rxjs';
 import { MAT_TOOLTIP_DEFAULT_OPTIONS } from '@angular/material/tooltip';
-import { StorybookTranslateModule } from '../i18n/i18n.module';
+import { tap } from 'rxjs/operators';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+export function initTranslations(
+  translateService: TranslateService,
+  httpClient: HttpClient
+) {
+  return () =>
+    httpClient
+      .get('https://i18n.hibob.com/bob-shared/en.json')
+      .pipe(
+        tap((data) => {
+          translateService.setTranslation('en', data, true);
+          translateService.setDefaultLang('en');
+          translateService.use('en');
+        })
+      )
+      .toPromise();
+}
 
 @NgModule({
   declarations: [StoryBookLayoutComponent],
   imports: [
     CommonModule,
-    StorybookTranslateModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
     TypographyModule,
     StatsModule,
   ],
   exports: [StoryBookLayoutComponent],
   providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initTranslations,
+      deps: [TranslateService, HttpClient],
+      multi: true,
+    },
     UtilsService,
     {
       provide: MAT_TOOLTIP_DEFAULT_OPTIONS,
