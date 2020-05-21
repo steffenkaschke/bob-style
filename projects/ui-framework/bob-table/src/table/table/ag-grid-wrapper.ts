00ffae08218d4330abdf28a0dfb5f4dea8f4c95a
Doron Cyngiser <doron.cy@gmail.com>
2020-05-21 09:45:06 +0300

    update ag-grid license (#1528)

--- projects/ui-framework/bob-table/src/table/table/ag-grid-wrapper.ts
+++ projects/ui-framework/bob-table/src/table/table/ag-grid-wrapper.ts
@@ -1,16 +1,14 @@
-import {RowNode, GridOptions} from 'ag-grid-community';
-import * as agGrid from 'ag-grid-enterprise';
+import { RowNode, GridOptions } from 'ag-grid-community';
+import { LicenseManager } from 'ag-grid-enterprise';
 
 const LICENSE_KEY =
-  'hibob_Bob_1Devs_1Deployment_23_April_2020__MTU4NzU5NjQwMDAwMA==5b77134bf43e27e7f8ccb20bdfa3c155';
-
+  'CompanyName=hibob,LicensedApplication=BOB,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=1,LicensedProductionInstancesCount=1,AssetReference=AG-008172,ExpiryDate=19_May_2021_[v2]_MTYyMTM3ODgwMDAwMA==9881948aa1a535a25dcd3c056897f090';
 
 export class AgGridWrapper {
-
   public gridOptions: GridOptions;
 
   constructor() {
-    agGrid.LicenseManager.setLicenseKey(LICENSE_KEY);
+    LicenseManager.setLicenseKey(LICENSE_KEY);
   }
 
   public setGridOptions(gridOptions: GridOptions) {
@@ -48,5 +46,4 @@ export class AgGridWrapper {
   public deselectAll(): void {
     this.gridOptions.api.deselectAll();
   }
-
 }
