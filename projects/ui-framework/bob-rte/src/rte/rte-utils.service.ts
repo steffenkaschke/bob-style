df666fea919e62e763ad4ff95bf722deb2806776
Omri Hecht <omrihecht@gmail.com>
2020-05-25 16:20:42 +0300

    rte bug fix (#1539)

--- projects/ui-framework/bob-rte/src/rte/rte-utils.service.ts
+++ projects/ui-framework/bob-rte/src/rte/rte-utils.service.ts
@@ -1,19 +1,20 @@
 import { escapeSafe } from 'bob-style';
 import { RteMentionsOption } from './rte.interface';
-import { Injectable, Sanitizer, SecurityContext } from '@angular/core';
+import { Injectable, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RteUtilsService {
-  constructor(private sanitizer: Sanitizer) {
+  constructor(private sanitizer: DomSanitizer) {
   }
 
   getSanitizeMentions(mentionsList: RteMentionsOption[]): RteMentionsOption[] {
     return mentionsList.map(mentionListItem => {
       return {
         displayName: escapeSafe(mentionListItem.displayName),
-        link: this.sanitizer.sanitize(SecurityContext.URL, mentionListItem.link),
+        link: mentionListItem.link && this.sanitizer.sanitize(SecurityContext.URL, mentionListItem.link),
         avatar: escapeSafe(mentionListItem.avatar),
         attributes: mentionListItem.attributes &&
           Object.keys(mentionListItem.attributes).reduce((acc, key) => {
