3258167e980c9f758d24e74b50900f5922eb9747
Pasha Golovin <pasha@galloween.net>
2020-07-26 14:23:42 +0300

    Pavel/masonry - cleanup (#1647)
    * InputSubject decorator, experimental Masonry layout
    
    * smarter Masonry
    
    * cleanup
    
    Co-authored-by: Pasha Golovin <psh.glvn@gmail.com>

--- projects/ui-framework/src/lib/layout/masonry/masonry.module.ts
+++ projects/ui-framework/src/lib/layout/masonry/masonry.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MasonryLayoutComponent } from './masonry.component';
+import { MasonryService } from './masonry.service';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [MasonryLayoutComponent],
   exports: [MasonryLayoutComponent],
+  providers: [MasonryService],
 })
 export class MasonryLayoutModule {}
