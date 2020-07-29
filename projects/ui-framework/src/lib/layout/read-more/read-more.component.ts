29bde2d2e3eb4dacbcdf65208e4ce9f9a6585e02
Pasha Golovin <psh.glvn@gmail.com>
2020-07-29 23:31:25 +0300

    little better read-more

--- projects/ui-framework/src/lib/layout/read-more/read-more.component.ts
+++ projects/ui-framework/src/lib/layout/read-more/read-more.component.ts
@@ -5,20 +5,38 @@ import {
   OnDestroy,
   Component,
   NgZone,
+  Output,
+  EventEmitter,
+  HostListener,
 } from '@angular/core';
-import { ReadMoreType } from './read-more.enum';
 import { DOMhelpers } from '../../services/html/dom-helpers.service';
-import {
-  ResizeObserverInstance,
-  WindowRef,
-  WindowLike,
-} from '../../services/utils/window-ref.service';
 import { closestDivisable } from '../../services/utils/functional-utils';
+import { Subscription, merge, of, Observable } from 'rxjs';
+import { tap, throttleTime, filter, take, skip } from 'rxjs/operators';
+import { MutationObservableService } from '../../services/utils/mutation-observable';
 import { UtilsService } from '../../services/utils/utils.service';
-import { InputObservable } from '../../services/utils/decorators';
-import { Subscription, merge, Observable, Subject } from 'rxjs';
-import { tap, skip, throttleTime, filter } from 'rxjs/operators';
 import { outsideZone } from '../../services/utils/rxjs.operators';
+import { InputObservable } from '../../services/utils/decorators';
+
+export interface ReadMoreConfig {
+  maxLines?: number;
+  maxHeight?: number;
+  expandable?: boolean;
+  watchClicks?: 'text' | 'read-more' | boolean;
+  expectChanges?: boolean;
+  trustCssVars?: boolean;
+  dynamicFontSize?: boolean;
+}
+
+export const READ_MORE_CONFIG_DEF: ReadMoreConfig = {
+  maxLines: 10,
+  maxHeight: null,
+  expandable: true,
+  watchClicks: false,
+  expectChanges: false,
+  trustCssVars: true,
+  dynamicFontSize: false,
+};
 
 @Component({
   selector: 'b-read-more',
@@ -32,128 +50,150 @@ import { outsideZone } from '../../services/utils/rxjs.operators';
       class="read-more-button mrg-t-8"
       [color]="'primary'"
       [text]="'Read More'"
-      (clicked)="onReadMoreClicked()"
+      (clicked)="onReadMoreClicked($event)"
     ></b-text-button>
   `,
 })
 export class ReadMoreComponent implements OnInit, OnDestroy {
   constructor(
-    private windowRef: WindowRef,
     private host: ElementRef,
-    private utilsService: UtilsService,
+    private zone: NgZone,
     private DOM: DOMhelpers,
-    private zone: NgZone
+    private utilsService: UtilsService,
+    private mutationObservableService: MutationObservableService
   ) {
-    this.nativeWindow = this.windowRef.nativeWindow;
     this.hostEl = this.host.nativeElement;
   }
 
-  private nativeWindow: WindowLike;
   private hostEl: HTMLElement;
   private contentEl: HTMLElement;
   private updater: Subscription;
-  private mutationObserver: MutationObserver;
-  private resizeObserver: ResizeObserverInstance;
-
   public needsReadMoreButton = false;
 
-  @Input() bReadMore = 10;
+  @InputObservable({ ...READ_MORE_CONFIG_DEF })
+  @Input('config')
+  config$: Observable<ReadMoreConfig>;
+  private config: ReadMoreConfig = READ_MORE_CONFIG_DEF;
 
-  // tslint:disable-next-line: no-input-rename
-  @InputObservable(null)
-  @Input('maxHeight')
-  maxHeight$: Observable<number>;
+  @Output() clicked: EventEmitter<'text' | 'read-more'> = new EventEmitter<
+    'text' | 'read-more'
+  >();
 
-  private maxHeight: number;
-
-  private changeDetection$: Subject<void> = new Subject<void>();
+  @HostListener('click.outside-zone')
+  onHostClick() {
+    if (
+      this.clicked.observers.length &&
+      this.config?.watchClicks !== 'read-more' &&
+      this.config?.watchClicks !== false
+    ) {
+      this.zone.run(() => {
+        this.clicked.emit('text');
+      });
+    }
+  }
 
   ngOnInit(): void {
+    this.contentEl = this.hostEl.children[0] as HTMLElement;
+
     this.updater = merge(
-      this.maxHeight$.pipe(
-        tap((maxHeight: number) => {
-          this.maxHeight = maxHeight;
+      this.config$.pipe(
+        tap((config: ReadMoreConfig) => {
+          this.config = { ...READ_MORE_CONFIG_DEF, ...config };
+          this.DOM.setAttributes(this.hostEl, {
+            'data-clickable': this.config.watchClicks,
+          });
         })
       ),
-      this.utilsService.getResizeEvent().pipe(outsideZone(this.zone), skip(1)),
-      this.changeDetection$
+
+      this.mutationObservableService
+        .getMutationObservable(this.hostEl, {
+          characterData: true,
+          childList: true,
+          subtree: true,
+          attributes: false,
+          mutations: 'processed',
+        })
+        .pipe(this.config?.expectChanges ? tap() : take(3)),
+
+      this.mutationObservableService.getResizeObservervable(this.hostEl, {
+        watch: this.config?.dynamicFontSize ? 'both' : 'height',
+        threshold: 5,
+      }),
+
+      this.utilsService.getResizeEvent().pipe(
+        outsideZone(this.zone),
+        skip(1),
+        filter(() => Boolean(this.config?.dynamicFontSize))
+      )
     )
       .pipe(
-        throttleTime(100, undefined, {
+        throttleTime(200, undefined, {
           leading: false,
           trailing: true,
         }),
-        filter(() => Boolean(this.hostEl?.children.length)),
-        tap(() => {
-          if (!this.contentEl) {
-            this.contentEl = this.hostEl?.children[0] as HTMLElement;
-          }
-        })
+        filter(() => Boolean(this.contentEl?.children.length))
       )
       .subscribe(() => {
         this.checkStuff();
       });
-
-    this.mutationObserver = new MutationObserver(() => {
-      this.changeDetection$.next();
-    });
-
-    this.resizeObserver = new this.nativeWindow.ResizeObserver(() => {
-      this.changeDetection$.next();
-    });
-
-    this.mutationObserver.observe(this.hostEl, {
-      childList: true,
-      subtree: true,
-      characterData: true,
-      attributes: false,
-    });
-    this.resizeObserver.observe(this.hostEl);
   }
 
   checkStuff() {
-    this.needsReadMoreButton =
-      this.contentEl.scrollHeight > this.hostEl.offsetHeight;
+    this.DOM.setAttributes(this.hostEl, {
+      'data-readmore':
+        (this.needsReadMoreButton =
+          this.contentEl.scrollHeight > this.contentEl.offsetHeight) + '',
+    });
 
     let deepEl = this.contentEl;
-
     while (deepEl.children.length === 1) {
       deepEl = deepEl.children[0] as HTMLElement;
     }
-
     const textProps = this.DOM.getElementTextProps(deepEl);
 
     this.DOM.setCssProps(this.contentEl, {
-      '--max-height': this.maxHeight
-        ? closestDivisable(
-            this.maxHeight,
-            textProps.fontSize * textProps.lineHeight
-          ) + 'px'
-        : null,
+      '--max-height':
+        Math.floor(
+          closestDivisable(
+            this.config.maxHeight ||
+              Math.floor(
+                textProps.lineHeight * textProps.fontSize * this.config.maxLines
+              ),
+            Math.floor(textProps.fontSize * textProps.lineHeight)
+          )
+        ) + 'px',
       '--line-height': textProps.lineHeight,
       '--font-size': textProps.fontSize + 'px',
-
-      'mask-image': null,
-      '-webkit-mask-image': null,
     });
   }
 
-  onReadMoreClicked() {
+  onReadMoreClicked(event: MouseEvent) {
+    if (
+      this.clicked.observers.length &&
+      this.config?.watchClicks !== 'text' &&
+      this.config?.watchClicks !== false
+    ) {
+      event.stopPropagation();
+      this.clicked.emit('read-more');
+    }
+
+    if (!this.config?.expandable) {
+      return;
+    }
+
     this.ngOnDestroy();
 
     this.DOM.setCssProps(this.contentEl, {
       transition: 'max-height 0.3s',
       '--max-height': this.contentEl.scrollHeight + 100 + 'px',
-      'mask-image': 'none',
-      '-webkit-mask-image': 'none',
     });
 
-    this.needsReadMoreButton = false;
+    this.DOM.setAttributes(this.hostEl, {
+      'data-readmore': this.needsReadMoreButton = false,
+    });
   }
 
   ngOnDestroy(): void {
-    this.mutationObserver?.disconnect();
-    this.resizeObserver?.disconnect();
     this.updater?.unsubscribe();
   }
 }
--- projects/ui-framework/src/lib/layout/read-more/read-more.module.ts
+++ projects/ui-framework/src/lib/layout/read-more/read-more.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReadMoreComponent } from './read-more.component';
 import { ButtonsModule } from '../../buttons/buttons.module';
+import { EventManagerPlugins } from '../../services/utils/eventManager.plugins';
 
 @NgModule({
   imports: [CommonModule, ButtonsModule],
   declarations: [ReadMoreComponent],
   exports: [ReadMoreComponent],
-  providers: [],
+  providers: [EventManagerPlugins[0]],
 })
 export class ReadMoreModule {}
