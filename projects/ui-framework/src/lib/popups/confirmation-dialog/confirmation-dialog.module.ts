d94ac23ad380a19616dbedc888c4f4721dad6c8f
Daniel Malkin <daniel.malkin@hibob.io>
2020-11-19 18:57:16 +0200

    add module

--- projects/ui-framework/src/lib/popups/confirmation-dialog/confirmation-dialog.module.ts
+++ projects/ui-framework/src/lib/popups/confirmation-dialog/confirmation-dialog.module.ts
@@ -6,10 +6,11 @@ import { ButtonsModule } from '../../buttons/buttons.module';
 import { TypographyModule } from '../../typography/typography.module';
 import { InputModule } from '../../form-elements/input/input.module';
 import { DeleteConfirmationDialogComponent } from './delete-confirmation-dialog/delete-confirmation-dialog.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ConfirmationDialogComponent, DeleteConfirmationDialogComponent],
-  imports: [CommonModule, DialogModule, ButtonsModule, TypographyModule, InputModule],
+  imports: [CommonModule, DialogModule, ButtonsModule, TypographyModule, InputModule, TranslateModule],
   entryComponents: [ConfirmationDialogComponent],
 })
 export class ConfirmationDialogModule {}
