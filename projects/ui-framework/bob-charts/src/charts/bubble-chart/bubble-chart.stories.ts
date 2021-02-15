cd34a8eb3fa9eef1d9875d9e01bb2268d29af2ad
orividra <ori.vidra@hibob.com>
2021-02-15 14:46:38 +0200

    bubble chart

---
+++ projects/ui-framework/bob-charts/src/charts/bubble-chart/bubble-chart.stories.ts
@@ -0,0 +1,145 @@
+import { storiesOf } from '@storybook/angular';
+import {
+  boolean,
+  number,
+  object,
+  select,
+  text,
+  withKnobs,
+} from '@storybook/addon-knobs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentGroupType } from '../../../../src/lib/consts';
+import { StoryBookLayoutModule } from '../../../../src/lib/story-book-layout/story-book-layout.module';
+import { ChartsModule } from '../charts.module';
+import { BUBBLE_CHART_CATEGORIES_MOCK, BUBBLE_CHART_DATA_MOCK } from '../chart.mock';
+import { ChartLegendPositionEnum } from '../charts.interface';
+
+const story = storiesOf(ComponentGroupType.Charts, module).addDecorator(
+  withKnobs
+);
+const template = `<b-bubble-chart
+        [data]="data"
+        [preTooltipValue]="preTooltipValue"
+        [postTooltipValue]="postTooltipValue"
+        [showDataLabels]="showDataLabels"
+        [legend]="legend"
+        [legendPosition]="legendPosition"
+        [colorPalette]="colorPalette"
+        [height]="height"
+        [title]="title"
+        [pointFormat]="pointFormat"
+        [xLabels]="xLabels"
+        [yLabels]="yLabels"
+        [xSize]="xSize"
+        [ySize]="ySize"
+        [xTitle]="xTitle"
+        [yTitle]="yTitle"
+        [color]="color"
+        #chart>
+</b-bubble-chart>
+<button (click)="chart.exportChart(downloadChart)">download</button>`;
+
+const storyTemplate = `
+<b-story-book-layout [title]="'Bubble Chart'">
+    <div>${template}</div>
+</b-story-book-layout>
+`;
+
+const note = `
+  ## Bubble Chart
+
+  #### Module
+  *ChartModule*
+  from <u>'bob-style/bob-charts'</u>
+
+  \`\`\`
+  import { ChartModule } from 'bob-style/bob-charts';
+  \`\`\`
+
+  ~~~
+  ${template}
+  ~~~
+
+  #### Properties
+  Name | Type | Description | Default value
+  --- | --- | --- | ---
+  *name | string | name of series | &nbsp;
+  *data | | series data array for chart | &nbsp;
+  legend (optional) | boolean | shows legend | false
+  colorPalette (optional) | string[] | color palette array | default array of colors
+  height (optional) | number | height of chart | 500
+  title (optional) | string | title of chart | &nbsp;
+  pointFormat (optional) | string | tooltip formatter | {series.name}: <b>{point.percentage:.1f}%</b>
+  xLabels | string[] | x series labels |
+  yLabels | string[] | y series labels |
+  xTitle | string | x series title |
+  yTitle | string | y series title |
+  xSize | number | x series size |
+  ySize | number | y series size |
+  color | string | bubbles color | #A4C9EF
+`;
+
+story.add(
+  'Bubble chart',
+  () => {
+    return {
+      template: storyTemplate,
+      props: {
+        downloadChart: select(
+          'downloadChart',
+          ['application/pdf', 'image/jpeg', 'image/png', 'image/svg+xml'],
+          'image/jpeg'
+        ),
+        legend: boolean('legend', true),
+        legendPosition: select(
+          'legendPosition',
+          Object.values(ChartLegendPositionEnum),
+          ChartLegendPositionEnum.BOTTOM
+        ),
+        name: text('name', 'bubbles'),
+        showDataLabels: boolean('showDataLabels', false),
+        preTooltipValue: text('preTooltipValue', ''),
+        postTooltipValue: text('postTooltipValue', ''),
+        title: text('title', ''),
+        height: number('height', 200),
+        xTitle: text('xTitle', ''),
+        yTitle: text('yTitle', ''),
+        xSize: number('xSize', 3),
+        ySize: number('ySize', 3),
+        xLabels: object('xLabels', BUBBLE_CHART_CATEGORIES_MOCK),
+        yLabels: object('yLabels', BUBBLE_CHART_CATEGORIES_MOCK),
+        color: text('color', '#A4C9EF'),
+        data: object('data', BUBBLE_CHART_DATA_MOCK),
+        colorPalette: object('colorPalette', [
+          '#CC2E4E',
+          '#87233D',
+          '#DB8962',
+          '#FEA54A',
+          '#FECC4A',
+          '#8F4A67',
+          '#D2728A',
+          '#D295A4',
+          '#E0ACAC',
+          '#BF8A78',
+          '#C0755A',
+          '#866161',
+          '#663E4E',
+          '#574285',
+          '#6969C6',
+          '#556E8A',
+          '#789BC2',
+          '#9BC7FA',
+          '#6DC3BC',
+          '#82D9B1',
+          '#959595',
+          '#616161',
+          '#313131',
+        ]),
+      },
+      moduleMetadata: {
+        imports: [StoryBookLayoutModule, BrowserAnimationsModule, ChartsModule],
+      },
+    };
+  },
+  { notes: { markdown: note } }
+);
---
+++ projects/ui-framework/bob-charts/src/charts/bubble-chart/bubble.chart.component.ts
@@ -0,0 +1,87 @@
+import {
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  NgZone,
+} from '@angular/core';
+import { ChartCore } from '../chart/chart-core';
+import { SeriesBubbleOptions } from 'highcharts';
+import { ChartTypesEnum } from '../charts.enum';
+
+@Component({
+  selector: 'b-bubble-chart',
+  templateUrl: '../chart/chart.component.html',
+  styleUrls: ['../chart/chart.component.scss'],
+})
+export class BubbleChartComponent extends ChartCore implements OnChanges {
+
+  readonly type: ChartTypesEnum = ChartTypesEnum.Bubble;
+
+  @Input() xTitle: string;
+  @Input() yTitle: string;
+  @Input() xLabels: string[];
+  @Input() yLabels: string[];
+  @Input() xSize: number;
+  @Input() ySize: number;
+  @Input() color: string;
+  @Input() data: SeriesBubbleOptions[];
+
+  constructor(public cdr: ChangeDetectorRef, public zone: NgZone) {
+    super(cdr, zone);
+    this.sizeDefaults[0] = 450;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    super.ngOnChanges(changes);
+
+    this.updateOptions();
+    this.applyOnChange();
+  }
+
+  private updateOptions() {
+    this.chartOptions = {
+      chart: {
+        height: Math.abs(this.height),
+        plotBorderWidth: 1,
+      },
+      xAxis: {
+        startOnTick: true,
+        endOnTick: false,
+        gridLineWidth: 1,
+        title: {
+          text: this.xTitle,
+        },
+        max: this.xSize - 1,
+        min: 0,
+        categories: this.xLabels ?? [],
+      },
+      yAxis: {
+        startOnTick: true,
+        endOnTick: false,
+        gridLineWidth: 1,
+        title: {
+          text: this.yTitle,
+        },
+        max: this.ySize - 1,
+        min: 0,
+        categories: this.yLabels ?? [],
+      },
+      plotOptions: {
+        bubble: {
+          color: this.color,
+        },
+        series: {
+          dataLabels: {
+            enabled: true,
+            format: '{point.name}'
+          }
+        }
+      },
+      series: [
+        { data: this.data }
+      ] as any
+    };
+  }
+}
