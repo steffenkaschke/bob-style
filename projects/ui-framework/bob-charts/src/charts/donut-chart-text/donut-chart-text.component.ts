adf59f8911744996be512ba11cc9b7ffde1e9404
Pasha Golovin <pasha@galloween.net>
2021-02-22 11:47:30 +0200

    Pavel/ donut-chart-text update (#1964)
    * misc
    
    * bump version
    
    * ss
    
    * ss
    
    * remove console log
    
    * littlest things
    
    * simpleUID update
    
    * disable multi-select ValueShowcase
    
    * some Sets methods
    
    * bump version
    
    * ss
    
    * misc
    
    * ss
    
    * autocomplete off
    
    * fix table 3dots button dissappearing on menu open
    
    * dialog header border on scroll
    
    * service/providers cleanup
    
    * ee showcase shuffle option
    
    * bump version
    
    * absolutely useless work
    
    * bump version
    
    * ss
    
    * PlaceholdersConverterService providedIn root
    
    * little things
    
    * ss
    
    * new icons
    
    * chip input upd, icons upd
    
    * normalized search
    
    * ss
    
    * list search single group option fix
    
    * bump version
    
    * ft
    
    * more fixes
    
    * fixing date-picker interface
    
    * new icons
    
    * bump version
    
    * ss
    
    * ss
    
    * ss
    
    * new grid classes
    
    * b-panel scroll
    
    * list avatar - enable bgcolor + no icon
    
    * some rte paste cleanup enhabcement
    
    * bump version
    
    * ss
    
    * additional control
    
    * ss
    
    * another autocomplete attempt
    
    * fix imports
    
    * simple collapsible
    
    * collapsible update
    
    * same
    
    * remove console.log
    
    * legend component
    
    * bump version
    
    * ss
    
    * fix test
    
    * fix legend without values
    
    * ss
    
    * better Legend
    
    * types fix
    
    * move radio mixin
    
    * froala update, little fixes
    
    * various updates
    
    * bump version
    
    * ss
    
    * misc
    
    * ss
    
    * lightbox
    
    * dialog
    
    * link
    
    * bump version
    
    * ft
    
    * misc
    
    * media-embed upd
    
    * bump version
    
    * ft
    
    * ft
    
    * click outside directive
    
    * WindowKeydownDirective
    
    * InViewDirective
    
    * ss
    
    * bump version
    
    * collapsible little upd
    
    * ss
    
    * fix small list padding
    
    * collapsible - icon size
    
    * bump version
    
    * ss
    
    * little things
    
    * lightbox - internal window messages listener (to close lightbox & show alerts)
    
    * button disabled icon color
    
    * versions bump
    
    * ss
    
    * version bump
    
    * donut-chart-text to extend chart-core
    
    * bump version
    
    Co-authored-by: Deployment Bot <bot@hibob.io>

--- projects/ui-framework/bob-charts/src/charts/donut-chart-text/donut-chart-text.component.ts
+++ projects/ui-framework/bob-charts/src/charts/donut-chart-text/donut-chart-text.component.ts
@@ -1,26 +1,24 @@
 import {
+  AfterViewInit,
   ChangeDetectorRef,
   Component,
   ElementRef,
   Input,
+  NgZone,
   OnChanges,
   SimpleChanges,
   ViewChild,
 } from '@angular/core';
-import { Options, SeriesPieDataOptions } from 'highcharts';
-import {
-  ChartLegendPositionEnum,
-  ChartTooltipTemplateFormatter,
-  ChartTooltipValueFormatter,
-} from '../charts.interface';
-import { applyChanges, pass, DonutSize } from 'bob-style';
+import { ChartLegendPositionEnum } from '../charts.interface';
+import { applyChanges, DonutSize } from 'bob-style';
 import {
   DONUT_CHART_WTEXT_ANIM_DURATION,
-  CHART_CORE_COLORPALETTE_DEF,
-  CHART_CORE_POINTFORMAT_DEF,
   DONUT_CHART_WTEXT_SIZE_DEFS,
-  CHART_CORE_TOOLTIP_TEMPLATE_DEF,
 } from '../charts.const';
+import { ChartCore } from '../chart/chart-core';
+import { ChartTypesEnum } from '../charts.enum';
+import { SeriesPieDataOptions } from 'highcharts';
+import { PieChartComponent } from '../pie-chart/pie-chart.component';
 
 export const debounce = <F extends (...args: any[]) => any>(
   func: F,
@@ -41,61 +39,55 @@ export const debounce = <F extends (...args: any[]) => any>(
   templateUrl: './donut-chart-text.component.html',
   styleUrls: ['./donut-chart-text.component.scss'],
 })
-export class DonutChartTextComponent implements OnChanges {
-  constructor(private cdr: ChangeDetectorRef) {}
+export class DonutChartTextComponent extends ChartCore
+  implements OnChanges, AfterViewInit {
+  constructor(public cdr: ChangeDetectorRef, public zone: NgZone) {
+    super(cdr, zone);
+    this.legendPosition = ChartLegendPositionEnum.RIGHT;
+    this.sizeDefaults = DONUT_CHART_WTEXT_SIZE_DEFS;
+  }
 
-  @ViewChild('chart', { read: ElementRef }) chartComponent: ElementRef;
+  @ViewChild(PieChartComponent, { read: ElementRef })
+  chartElRef: ElementRef<HTMLElement>;
+  @ViewChild(PieChartComponent)
+  chart: PieChartComponent;
   @ViewChild('text', { read: ElementRef }) textContainer: ElementRef;
 
   @Input() data: SeriesPieDataOptions[];
   @Input() name: string;
-  @Input() height = 300;
-  @Input() donutInnerSize = 80;
-  @Input() title: string = null;
-  @Input() legend = false;
-  @Input() legendPosition: ChartLegendPositionEnum =
-    ChartLegendPositionEnum.RIGHT;
-  @Input() pointFormat = CHART_CORE_POINTFORMAT_DEF;
-  @Input() extraOptions: Options = {};
-  @Input() preTooltipValue = '';
-  @Input() postTooltipValue = '';
-  @Input() colorPalette: string[] = [...CHART_CORE_COLORPALETTE_DEF];
+  @Input() donutInnerSize = this.sizeDefaults[1];
   @Input() donutSize: DonutSize;
-
-  @Input() tooltipValueFormatter: ChartTooltipValueFormatter = pass;
-  @Input()
-  tooltipTemplate: ChartTooltipTemplateFormatter = CHART_CORE_TOOLTIP_TEMPLATE_DEF;
 
   // tslint:disable-next-line: member-ordering
   public textStyle: any;
+  readonly type = ChartTypesEnum.Pie;
 
   ngOnChanges(changes: SimpleChanges): void {
     applyChanges(
       this,
       changes,
       {
-        height: DONUT_CHART_WTEXT_SIZE_DEFS[0],
-        donutInnerSize: DONUT_CHART_WTEXT_SIZE_DEFS[1],
+        height: this.sizeDefaults[0],
+        donutInnerSize: this.sizeDefaults[1],
+        donutSize: null,
         legendPosition: ChartLegendPositionEnum.RIGHT,
-        pointFormat: CHART_CORE_POINTFORMAT_DEF,
-        extraOptions: {},
-        colorPalette: [...CHART_CORE_COLORPALETTE_DEF],
-        preTooltipValue: '',
-        postTooltipValue: '',
-        tooltipValueFormatter: pass,
-        tooltipTemplate: CHART_CORE_TOOLTIP_TEMPLATE_DEF,
       },
       [],
       true
     );
+
+    super.ngOnChanges(changes);
+  }
+
+  ngAfterViewInit() {
+    this.highChartRef = this.chart.highChartRef;
   }
 
   private calculateTextPosition() {
-    const chartWrapperRect = (this.chartComponent
-      .nativeElement as HTMLElement).getBoundingClientRect();
-    const chartRect = (this.chartComponent.nativeElement.querySelector(
-      '.highcharts-plot-background'
-    ) as HTMLElement).getBoundingClientRect();
+    const chartWrapperRect = this.chartElRef.nativeElement.getBoundingClientRect();
+    const chartRect = this.chartElRef.nativeElement
+      .querySelector('.highcharts-plot-background')
+      .getBoundingClientRect();
     this.textStyle = {
       top: `${chartRect.top - chartWrapperRect.top}px`,
       left: `${chartRect.left - chartWrapperRect.left}px`,
@@ -126,4 +118,7 @@ export class DonutChartTextComponent implements OnChanges {
       this.updateTextStyle();
     }, DONUT_CHART_WTEXT_ANIM_DURATION)();
   }
+
+  initialOptions() {}
+  applyOnChange() {}
 }
