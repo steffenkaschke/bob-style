b663dede07e3ee766ada7af62b5546c562feb349
Maxim Dicusari <maxim.dicusari@gmail.com>
2021-03-01 15:58:05 +0200

    - add the story for the color-picker component

---
+++ projects/ui-framework/src/lib/form-elements/color-picker/color-picker.stories.ts
@@ -0,0 +1,81 @@
+import { storiesOf } from '@storybook/angular';
+import {
+  text,
+  boolean,
+  withKnobs,
+} from '@storybook/addon-knobs';
+import { ComponentGroupType } from '../../consts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoryBookLayoutModule } from '../../story-book-layout/story-book-layout.module';
+
+// @ts-ignore: md file and not a module
+import formElemsPropsDoc from '../form-elements.properties.md';
+// @ts-ignore: md file and not a module
+import inputElemsPropsDoc from '../input.properties.md';
+import { FormElementSize } from '../form-elements.enum';
+import { FormElementsCommonProps } from '../form-elements.stories.common';
+import { ColorPickerModule } from './color-picker.module';
+
+const story = storiesOf(ComponentGroupType.FormElements, module).addDecorator(
+  withKnobs
+);
+
+const template = `
+  <b-colorpicker
+  [value]="value"
+  [label]="label"
+  [placeholder]="placeholder"
+  [description]="description"
+  [hideLabelOnFocus]="hideLabelOnFocus"
+  [disabled]="disabled"
+  [required]="required"
+  [readonly]="readonly"
+  [hintMessage]="hintMessage"
+  [warnMessage]="warnMessage"
+  [errorMessage]="errorMessage"
+  [focusOnInit]="focusOnInit">
+</b-colorpicker>
+`;
+
+
+const storyTemplate = `
+<b-story-book-layout [title]="'Input'">
+  <div style="max-width: 300px;">
+    ${template}
+  </div>
+</b-story-book-layout>
+`;
+
+const note = `
+  ## ColorPicker Element
+  #### Module
+  *ColorPickerModule* or *FormElementsModule*
+
+  ~~~
+  ${template}
+  ~~~
+
+  *Note*: Component for selecting the color in the HEX format by the color picker dropdown, or properly via the input.
+  Can be used inside the forms.
+
+  ${inputElemsPropsDoc}
+
+  ${formElemsPropsDoc}
+`;
+story.add(
+  'Color Picker',
+  () => {
+    return {
+      template: storyTemplate,
+      props: {
+        value: text('value (text input)', '#C6C6C6'),
+        ...FormElementsCommonProps('Input label', 'Input placeholder'),
+        showCharCounter: boolean('showCharCounter', true),
+      },
+      moduleMetadata: {
+        imports: [BrowserAnimationsModule, ColorPickerModule, StoryBookLayoutModule],
+      },
+    };
+  },
+  { notes: { markdown: note } }
+);
