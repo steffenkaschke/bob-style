cddf026036e308a16e5e1d3ead639843092c1965
Miki Ashkenazi <miki.ashkenazi@hibob.io>
2021-03-14 14:47:55 +0200

    changed font-weight and added title prop

--- projects/ui-framework/src/lib/indicators/empty-state/empty-state.interface.ts
+++ projects/ui-framework/src/lib/indicators/empty-state/empty-state.interface.ts
@@ -2,6 +2,7 @@ import { Button } from '../../buttons/buttons.interface';
 import { Icons, IconSize } from '../../icons/icons.enum';
 
 export interface EmptyStateConfig {
+  title?: string;
   text?: string;
   icon: Icons;
   iconSize?: IconSize;
